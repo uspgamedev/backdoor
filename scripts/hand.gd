6701c1192725582885f0731aca5f472611af4e60
Kazuo256 <kazuo256@gmail.com>
2016-03-31 18:30:09 -0300

    Player hand
    Highlight focused card that will be used

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -4,6 +4,7 @@ extends Node2D
 const CardSprite = preload("res://card.xscn")
 var player
 var cards
+var focus
 
 func _ready():
 	player = get_node("/root/Node2D/TileMap/Hero")
@@ -17,10 +18,18 @@ func _on_player_draw(card):
 	card_sprite.get_node("Name").set_text(card.name)
 	cards.append(card_sprite)
 	add_child(card_sprite)
+	if focus == null:
+		focus = 0
 
 func _process(delta):
 	var n = cards.size()
 	var angle = 30/(n+1)
+	for i in range(cards.size()):
+		var card = cards[i]
+		if i != focus:
+			card.get_node("Border").set_modulate(Color(0,0,0))
+		else:
+			card.get_node("Border").set_modulate(Color(1,1,1))
 	for i in range(n-1, -1, -1):
 		var transform = Matrix32(-deg2rad((i - (n-1)*.5)*angle), Vector2(0,1024))
 		cards[i].set_transform(transform.translated(Vector2(0, -1024)))
