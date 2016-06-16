61d37b228b63dcd385ddbf4521f1cf8d384c38e3
Rafael Correia <rafaeljpc@gmail.com>
2016-06-16 14:26:41 -0300

    Implementation #26 card colors according to the type

--- model/card_ref.gd
+++ model/card_ref.gd
@@ -1,6 +1,16 @@
 
 extends Node
 
+class CARD_TYPE:
+	const ARCANE = 0
+	const ATHELITCS = 1
+	const ENGINEERING = 2
+
+export(int, "ARCANE", "ATHELITCS", "ENGINEERING") var card_type = 0
+
+func get_card_type():
+	return card_type
+
 func get_time_cost():
 	return 100
 
--- scenes/hud/card_sprite.gd
+++ scenes/hud/card_sprite.gd
@@ -2,7 +2,25 @@
 extends Node2D
 
 const CardScene = preload("res://scenes/hud/card.xscn")
+const CARD_TYPE = preload("res://model/card_ref.gd").CARD_TYPE
 const ANGLE = -atan2(1,2)
+const BG_COLOR = "bg_color"
+const FG_COLOR = "fg_color"
+
+const COLOR_DICT = {
+	CARD_TYPE.ARCANE: {
+			BG_COLOR: Color("000e36"),
+			FG_COLOR: Color("6277ff")
+		},
+	CARD_TYPE.ATHELITCS: {
+			BG_COLOR: Color("3d0b0b"),
+			FG_COLOR: Color("d95763")
+		},
+	CARD_TYPE.ENGINEERING: {
+			BG_COLOR: Color("133b0b"),
+			FG_COLOR: Color("7fd95b")
+		}
+}
 
 var card
 
@@ -10,6 +28,8 @@ static func create(card):
 	var card_sprite = CardScene.instance()
 	card_sprite.card = card
 	card_sprite.get_node("Name").set_text(card.get_name())
+	card_sprite.get_node("Background").set_modulate(COLOR_DICT[card.card_ref.get_card_type()][BG_COLOR])
+	card_sprite.get_node("Subborder").set_modulate(COLOR_DICT[card.card_ref.get_card_type()][FG_COLOR])
 	return card_sprite
 
 func select():
