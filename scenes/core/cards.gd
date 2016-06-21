058e47b470a20db816c0005128c99923a6d6e29e
Rafael Correia <rafaeljpc@gmail.com>
2016-06-21 01:10:37 -0300

    Fixing Loading upgrades bug

--- scenes/core/cards.gd
+++ scenes/core/cards.gd
@@ -4,8 +4,7 @@ extends Node
 const Card = preload("res://model/card_ref.gd")
 
 func get_card_id(card):
-	if not card extends Card:
-		assert(false)
+	assert(card extends Card)
 	for i in range(get_child_count()):
 		if card == get_child(i):
 			return i
