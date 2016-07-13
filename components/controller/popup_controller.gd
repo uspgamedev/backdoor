e515f672c56f997d5f053705e5144e8899f39fb8
Kazuo256 <kazuo256@gmail.com>
2016-07-13 10:37:00 -0300

    Syntax error
    Fix syntax error (extra parenthesis)

--- components/controller/popup_controller.gd
+++ components/controller/popup_controller.gd
@@ -3,7 +3,7 @@ extends "res://components/controller/default_controller.gd"
 
 export(String) var path = ".."
 
-func consume_input_key(event):)
+func consume_input_key(event):
   if not enabled:
     return
 
