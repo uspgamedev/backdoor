b1d1aab5d74ab4e639127e950e71a3079a8c778f
Kazuo256 <kazuo256@gmail.com>
2016-07-13 18:40:30 -0300

    Map generation
    Remove empty line

--- model/captains_log.gd
+++ model/captains_log.gd
@@ -38,7 +38,6 @@ func create_route():
 	var walls = map_node.get_node("walls") #FIXME
 	while (walls.get_cell(pos.x, pos.y) != -1):
 		pos = Vector2(randi()%w, randi()%h)
-
 	var player_body = Body.new(1, "hero", pos, 10)
 	map_node.add_body(player_body)
 	map_node.add_actor(player_body, player)
