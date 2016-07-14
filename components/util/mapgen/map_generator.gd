2ccd14b454f5f31edb368aaa40222c94f7c46be0
Kazuo256 <kazuo256@gmail.com>
2016-07-14 13:50:57 -0300

    Map generation
    Add margin to map

--- components/util/mapgen/map_generator.gd
+++ components/util/mapgen/map_generator.gd
@@ -6,17 +6,20 @@ const Step = preload("res://components/util/mapgen/step.gd")
 const RandomStep = preload("res://components/util/mapgen/steps/add_random_wall.gd")
 const RoomStep = preload("res://components/util/mapgen/steps/carve_rooms.gd")
 const PatternStep = preload("res://components/util/mapgen/steps/pattern_filter.gd")
+const MarginStep = preload("res://components/util/mapgen/steps/add_margin.gd")
 
 var RANDOM_STEP = RandomStep.new()
 var ROOM_STEP = RoomStep.new()
 var GROW_STEP = PatternStep.load_from_file("growing")
 var CLEAN_STEP = PatternStep.load_from_file("cleaning")
 var BORDER_STEP = PatternStep.load_from_file("border")
+var MARGIN_STEP = MarginStep.new()
 
 var PIPELINE = [
   ROOM_STEP,
   #RANDOM_STEP, RANDOM_STEP, RANDOM_STEP,
   #GROW_STEP, GROW_STEP, GROW_STEP,
+  MARGIN_STEP,
   CLEAN_STEP, CLEAN_STEP, CLEAN_STEP,
   BORDER_STEP
 ]
