2866575b01d5f7b9c406238ed030178c6578ec5d
Eduardo Silva Araújo <duduktamg@hotmail.com>
2016-07-20 08:35:47 -0300

    Add Laser Beam card

--- model/cards/laser_beam.gd
+++ model/cards/laser_beam.gd
@@ -1,32 +1,53 @@
 
 extends "res://model/cards/card_skill.gd"
 
-const AREA = [[1,1,1,1,1]]
-const CENTER = Vector2(0,0)
 const RANGE = 5
+
+var horizontal = true
 
 func valid_target(actor, target):
   var map = get_node("/root/sector/map")
-  return map.is_empty_space(target)
+  var pos = actor.get_body_pos()
+  return map.is_empty_space(target) and \
+    (pos.x == target.x and pos.y != target.y and abs(pos.y - target.y) < RANGE) or \
+    (pos.y == target.y and pos.x != target.x and abs(pos.x - target.x) < RANGE)
 
-func get_area(pos, target):
-  if pos.x == target.x:
-    return [[1,1,1,1,1]]
+func get_area(actor_pos, target):
+  if actor_pos.x == target.x:
+    horizontal = true
+    return [[1],[1],[1],[1]]
+  else:
+    horizontal = false
+    return [[1,1,1,1]]
+
+func get_center(actor_pos, target):
+  var dist
+  if horizontal:
+    dist = target.y - actor_pos.y
+    if dist < 0:
+      dist = RANGE + dist
+    return Vector2(0, dist - 1)
+  else:
+    dist = target.x - actor_pos.x
+    if dist < 0:
+      dist = RANGE + dist
+    return Vector2(dist - 1, 0)
 
 func get_options(actor):
   return [
     { "type": "TARGET", "check": funcref(self, "valid_target"),
-    "aoe":{"format":AREA, "center":CENTER}}
+    "aoe":{"format": funcref(self, "get_area"), "center":funcref(self, "get_center")}}
   ]
 
 func evoke(actor, options):
   var map = get_node("/root/sector/map")
   var pos = options[0]
-  for i in range(AREA.size()):
-    for j in range(AREA[i].size()):
-      var target = AREA[i][j]
+  var area = get_area(actor.get_body_pos(), pos)
+  for i in range(area.size()):
+    for j in range(area[i].size()):
+      var target = area[i][j]
       if target == 1:
         var body = map.get_body_at(pos - CENTER + Vector2(j,i))
         if body != null:
           printt("LASERBEAM SHOT", i, j)
-          body.take_damage(20)
+          body.take_damage(15)
