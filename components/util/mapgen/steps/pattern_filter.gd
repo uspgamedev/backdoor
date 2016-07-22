7901a4c84ab6afac9274a673cd54bb3a65bc3f93
Kazuo256 <kazuo256@gmail.com>
2016-07-22 14:10:28 -0300

    Map generation performance
    Optimize map generation with paralelism. Aprox. 40% gain.

--- components/util/mapgen/steps/pattern_filter.gd
+++ components/util/mapgen/steps/pattern_filter.gd
@@ -1,7 +1,22 @@
 
 extends "res://components/util/mapgen/step.gd"
 
+const THREAD_NUM = 4
+
+const WINDOW = [
+  Vector2(-1,-1),
+  Vector2(-1,0),
+  Vector2(-1,1),
+  Vector2(0,-1),
+  Vector2(0,0),
+  Vector2(0,1),
+  Vector2(1,-1),
+  Vector2(1,0),
+  Vector2(1,1)
+]
+
 var rules_
+var sem_
 
 func _init(dict):
   rules_ = dict
@@ -16,21 +31,46 @@ static func load_from_file(pattern_name):
   file.close()
   return new(dict)
 
+func apply_part(args):
+  var map = args[0]
+  var grid = args[1]
+  var j0 = args[2]
+  var i0 = args[3]
+  var w = args[4]
+  var h = args[5]
+  printt("thread", args[2], args[3], args[2] + args[4] - 1, args[3] + args[5] - 1)
+  for i in range(i0, i0+h):
+    for j in range(j0, j0+w):
+      #sem_.wait()
+      grid.set_tile(i, j, map[i][j])
+      #sem_.post()
+      for key in rules_:
+        var patterns = rules_[key].patterns
+        var value = rules_[key].value
+        for pattern in patterns:
+          var change = true
+          for d in WINDOW:
+            var tile = map[i+d.y][j+d.x]
+            var pat = pattern[d.y+1][d.x+1]
+            if not ( pat == ANY or tile == pat or (pat == ANY_BUT_WALL and tile != WALL) ):
+              change = false
+              break
+          if change:
+            grid.set_tile(i, j, value)
+            break
+
 # override
 func apply(map, w, h):
   var map_grid = MapGrid.new(w, h, EMPTY)
-  for i in range(1, w - 1):
-    for j in range(1, h - 1):
-      map_grid.set_tile(i, j, map[i][j])
-      for key in rules_:
-        var rule = rules_[key]
-        for pattern in rule.patterns:
-          var change = true
-          for di in range(-1,2):
-            for dj in range(-1,2):
-              if not ( pattern[di+1][dj+1] == ANY or map[i+di][j+dj] == pattern[di+1][dj+1] or (pattern[di+1][dj+1] == ANY_BUT_WALL and map[i+di][j+dj] != WALL) ):
-                change = false
-          if change:
-            map_grid.set_tile(i, j, rule.value)
-            break
+  var threads = range(THREAD_NUM)
+  for i in range(THREAD_NUM):
+    threads[i] = Thread.new()
+  sem_ = Semaphore.new()
+  sem_.post()
+  for i in range(THREAD_NUM-1):
+    threads[i].start(self, "apply_part", [map, map_grid, 1, 1+i*h/THREAD_NUM, w-2, h/THREAD_NUM])
+  threads[THREAD_NUM-1].start(self, "apply_part", [map, map_grid, 1, 1+(THREAD_NUM-1)*h/THREAD_NUM, w-2, h/THREAD_NUM-2])
+  for i in range(THREAD_NUM):
+    threads[i].wait_to_finish()
+  sem_ = null
   return map_grid
