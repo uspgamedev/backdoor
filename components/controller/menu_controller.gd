e05bd1184615eb30ed6c3a4e9566580cd25a9c5f
orenjiakira <orenjiakira@gmail.com>
2016-09-03 21:19:52 -0300

    Clean menu_controller code

--- components/controller/menu_controller.gd
+++ components/controller/menu_controller.gd
@@ -1,7 +1,7 @@
 
 extends "res://components/controller/default_controller.gd"
 
-var choice = 1
+var choice
 var choice_list_size
 var cursor
 var saves_node
@@ -9,14 +9,14 @@ var saves_node
 onready var menu = get_parent()
 
 func setup():
+  choice = 1 # default to new route choice
   saves_node = menu.saves_node
   cursor = saves_node.get_node("cursor")
   choice_list_size = saves_node.get_child_count() - 1
-  #self.connect("selected", )
   self.enable()
-  self.update()
+  self.update_choice()
 
-func update():
+func update_choice():
   # updates cursor position
   # should play a sfx eventually too
   cursor.set_pos(Vector2(-32, ((choice-1) * 32) - 4))
@@ -24,26 +24,20 @@ func update():
   for btn in saves_node.get_children():
     if btn != cursor:
       if btn == saves_node.get_child(choice):
-        print(str(choice) + " is changing color!")
         btn.add_color_override("font_color", Color(234.0/255,166.0/255,81.0/255))
       else:
         btn.add_color_override("font_color", Color(1,1,1))
 
 func event_up():
   choice = ((choice + choice_list_size - 2) % choice_list_size) + 1
-  update()
+  update_choice()
 
 func event_down():
   choice = (choice % choice_list_size) + 1
-  update()
+  update_choice()
 
 func event_select():
   var selection = saves_node.get_child(choice)
-  if saves_node.get_child(choice) != cursor:
-    if selection.get("route_id"):
-      selection.emit_signal("selected")
-    else:
-      selection.emit_signal("selected")
-    self.disable()
-  else:
-    print("Error! This shouldn't happen! You can't select the cursor to load!")
+  assert(selection != cursor) # redundant check yay
+  selection.emit_signal("selected")
+  self.disable()
