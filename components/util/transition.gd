a7d45b44a2256792664565f9b5bc7376415c11ea
orenjiakira <orenjiakira@gmail.com>
2016-09-07 14:24:44 -0300

    Retouch and enhance transition node

--- components/util/transition.gd
+++ components/util/transition.gd
@@ -7,23 +7,42 @@ const INVISIBLE_BLACK = Color(42.0/255, 26.0/255, 31.0/255, 0)
 onready var tween = get_node("tween")
 onready var overlay = get_node("overlay")
 
-signal start_transition()
-signal end_transition()
+signal begin_fadeout()
+signal end_fadeout()
+signal begin_fadein()
+signal end_fadein()
+
+func reset_connection(signal_, node_, callback_):
+  disconnect(signal_, node_, callback_)
+
+func configure_fadeout(begin_node, begin_callback, end_node, end_callback):
+  connect("begin_fadeout", begin_node, begin_callback)
+  connect("begin_fadeout", self, "reset_connection", ["begin_fadeout", begin_node, begin_callback])
+  connect("end_fadeout", end_node, end_callback)
+  connect("end_fadeout", self, "reset_connection", ["end_fadeout", end_node, end_callback])
+
+func configure_fadein(begin_node, begin_callback, end_node, end_callback):
+  connect("begin_fadein", begin_node, begin_callback)
+  connect("begin_fadein", self, "reset_connection", ["begin_fadein", begin_node, begin_callback])
+  connect("end_fadein", end_node, end_callback)
+  connect("end_fadein", self, "reset_connection", ["end_fadein", end_node, end_callback])
 
 func fade_to_black(seconds):
-  emit_signal("start_transition")
-  tween.interpolate_method( overlay, "set_color", INVISIBLE_BLACK, BLACK, seconds, Tween.TRANS_QUAD, Tween.EASE_OUT, 0)
+  emit_signal("begin_fadeout")
+  print("fading start!")
+  tween.interpolate_method(overlay, "set_color", INVISIBLE_BLACK, BLACK, seconds, Tween.TRANS_QUAD, Tween.EASE_OUT, 0)
   tween.start()
   yield(tween, "tween_complete")
-  emit_signal("end_transition")
+  emit_signal("end_fadeout")
+  print("fading end!")
 
 func unfade_from_black(seconds):
-  emit_signal("start_transition")
+  emit_signal("begin_fadein")
   print("unfading start!")
-  tween.interpolate_method( overlay, "set_color", BLACK, INVISIBLE_BLACK, seconds, Tween.TRANS_QUAD, Tween.EASE_IN, 0)
+  tween.interpolate_method(overlay, "set_color", BLACK, INVISIBLE_BLACK, seconds, Tween.TRANS_QUAD, Tween.EASE_IN, 0)
   tween.start()
   yield(tween, "tween_complete")
-  emit_signal("end_transition")
+  emit_signal("end_fadein")
   print("unfading end!")
 
 func create_viewport_sized_polygon():
