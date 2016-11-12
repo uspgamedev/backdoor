06057cc6e62603aa4648b7e1f1847389f4ddc204
Kazuo256 <kazuo256@gmail.com>
2016-11-12 14:55:23 -0200

    [mapgen] Erase isolated dirt tiles

--- game/procedural/map/steps/grow_smooth.gd
+++ game/procedural/map/steps/grow_smooth.gd
@@ -21,6 +21,13 @@ func _init(where, which, min_count, max_count, size):
     for j in range(size):
       window[i*size + j] = Vector2(i - size/2, j - size/2)
 
+func pvt_count_neighbors(map,i,j):
+  var count = 0
+  for d in window:
+    if map[i + d.x][j + d.y] == which:
+      count += 1
+  return count
+
 func apply_part(args):
   var map = args[0]
   var grid = args[1]
@@ -31,14 +38,13 @@ func apply_part(args):
   for i in range(i0, i0+h):
     for j in range(j0, j0+w):
       grid.set_tile(i, j, map[i][j])
-      if grid.is_tile(i, j, where):
-        var count = 0
-        for d in window:
-          var tile = map[i + d.x][j + d.y]
-          if tile == which:
-            count += 1
-        if count >= min_count and count <= max_count:
-          grid.set_tile(i, j, which)
+      var count = pvt_count_neighbors(map, i, j)
+      if grid.is_tile(i, j, where) and \
+         count >= min_count and count <= max_count:
+        grid.set_tile(i, j, which)
+      elif grid.is_tile(i, j, which) and count < min_count:
+        grid.set_tile(i, j, where)
+
 
 # override
 func apply(map, w, h):
@@ -46,12 +52,9 @@ func apply(map, w, h):
   var threads = range(THREAD_NUM)
   for i in range(THREAD_NUM):
     threads[i] = Thread.new()
-  sem = Semaphore.new()
-  sem.post()
   for i in range(THREAD_NUM-1):
     threads[i].start(self, "apply_part", [map, map_grid, 1, 1+i*h/THREAD_NUM, w-2, h/THREAD_NUM])
   threads[THREAD_NUM-1].start(self, "apply_part", [map, map_grid, 1, 1+(THREAD_NUM-1)*h/THREAD_NUM, w-2, h/THREAD_NUM-2])
   for i in range(THREAD_NUM):
     threads[i].wait_to_finish()
-  sem = null
   return map_grid
