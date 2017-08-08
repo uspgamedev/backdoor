cf09eb63a5c4a40ccc0d9f58d0eae6c8179ee603
Kazuo256 <kazuo256@gmail.com>
2017-08-07 22:05:49 -0300

    Remove godot project

--- model/cards/card_item_armory.gd
+++
@@ -1,6 +0,0 @@
-extends "res://model/cards/card_item.gd"
-
-export(int) var absorption = 5
-
-func get_absorption():
-  return absorption
--- model/cards/heal.gd
+++
@@ -1,8 +0,0 @@
-
-extends "res://model/cards/card_skill.gd"
-
-func can_be_evoked(actor):
-  return true
-
-func evoke(actor, options):
-  actor.get_body().heal(5)
