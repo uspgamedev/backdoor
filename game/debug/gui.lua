1316514e20b820be0c625d8d829017500d0ed29f
Kazuo256 <kazuo256@gmail.com>
2017-08-12 17:44:52 -0300

    Refactor GUI stack, less stuff to remember

--- game/debug/gui.lua
+++ game/debug/gui.lua
@@ -12,36 +12,38 @@ function GUI:init()
   ELEMENT.init(self)
   self.stack = {}
   self.active = false
+  self.current_level = 1
 
 end
 
-function GUI:push(level, viewname, ...)
+function GUI:push(viewname, ...)
+  local level = self.current_level+1
   for i=level,#self.stack do
     self.stack[i] = nil
   end
-  self.stack[level] = view[viewname](level, ...)
-end
-
-function view.main(level)
+  local render = view[viewname](...)
   local x = tween.start((level-2)*200, (level-1)*200, 5)
-  return function(self)
+  self.stack[level] = function (self)
     imgui.SetNextWindowPos(x(), 200, "Always")
     imgui.SetNextWindowSizeConstraints(200, 10, 200, 400)
+    render(self)
+  end
+end
+
+function view.main()
+  return function(self)
     imgui.Begin("Actors", true, { "NoCollapse" })
     for actor,_ in pairs(Util.findSubtype 'actor') do
       if imgui.Button(actor.id) then
-        self:push(level+1, 'actor', actor)
+        self:push('actor', actor)
       end
     end
     imgui.End()
   end
 end
 
-function view.actor(level, actor)
-  local x = tween.start((level-2)*200, (level-1)*200, 5)
+function view.actor(actor)
   return function(self)
-    imgui.SetNextWindowPos(x(), 200, "Always")
-    imgui.SetNextWindowSizeConstraints(200, 10, 200, 400)
     imgui.Begin(actor.id, false, { "NoCollapse" })
     imgui.Text(("HP: %d"):format(actor:getBody():getHP()))
     imgui.End()
@@ -51,7 +53,8 @@ end
 
 function GUI:draw()
   if DEBUG and not self.active then
-    self:push(1, 'main')
+    self.current_level = 0
+    self:push('main')
     self.active = true
   elseif not DEBUG then
     self.stack = {}
@@ -63,7 +66,8 @@ function GUI:draw()
 
   imgui.NewFrame()
 
-  for _,view in ipairs(self.stack) do
+  for level,view in ipairs(self.stack) do
+    self.current_level = level
     view(self)
   end
 
