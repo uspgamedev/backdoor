75e1b8bad31469329c94f8357daf2e813069c744
orenjiakira <orenjiakira@gmail.com>
2017-08-17 16:17:49 -0300

    Implement maze-maker transformer

--- game/domain/transformers/maze.lua
+++ game/domain/transformers/maze.lua
@@ -1,18 +1,94 @@
 
-local Queue = require 'lux.common.Queue'
-local Rand = require 'domain.generators.helpers.random'
-local Vector2 = require 'cpml.modules.vec2'
+-- dependencies
+local Helpers = require 'lux.pack' 'domain.transformers.helpers'
+local Queue   = require 'lux.common.Queue'
+local Vector2  = require 'cpml.modules.vec2'
 
-function create(map, margin)
+local Rectangle  = Helpers.rect
+local random     = Helpers.random
+local schematics = Helpers.schematics
+
+return function (map, params)
   local progress = Queue(64)
-  local start = Vector2(
-    Rand.odd(margin + 1, map.getWidth() - margin),
-    Rand.odd(margin + 1, map.getHeight() - margin),
-  )
-  local mazeable = true
-  while mazeable do
-    --
+
+  local mw, mh = map.getMargins()
+  local minx = mw + 1
+  local miny = mh + 1
+  local maxx = map.getWidth() - mw
+  local maxy = map.getHeight() - mh
+  local dist = 2 * (params.double and 2 or 1)
+  local cardinals = {
+    dist * Vector2(+1,  0),
+    dist * Vector2( 0, +1),
+    dist * Vector2(-1,  0),
+    dist * Vector2( 0, -1),
+  }
+
+  local potentials = {}  -- { point, direction }
+  local maze_scheme = {} -- { point, direction }
+  local start
+
+  local function isValidPoint(point)
+    local FLOOR = 
+    local x, y = point.x, point.y
+    return map.isInsideMargins(x, y)
+      and map.get(x, y) ~= schematics.FLOOR
   end
+
+  local function setStartPoint()
+    local FLOOR = schematics.FLOOR
+    repeat start = Vector2(random.odd(minx, maxx), random.odd(miny, maxy))
+    until isValidPoint(start)
+  end
+
+  local function addPotentialMovements()
+    local insert = table.insert
+    for _, dir in ipairs(cardinals) do
+      local newpoint = start + dir
+      if isValidPoint(newpoint) then
+        insert(potentials, { start, dir })
+      end
+    end
+  end
+
+  local function getPotentialMovement()
+    local N = #potentials
+    local k = random.interval(1, N)
+    local movement = potentials[k]
+    potentials[k] = potentials[N]
+    potentials[N] = nil
+    return isValidPoint(movement[1] + movement[2]) and movement
+  end
+
+  local function generateMaze()
+    local insert = table.insert
+    local moveable
+    repeat
+      addPotentialMovements()
+      moveable = getPotentialMovement()
+      if moveable then
+        start = moveable[1] moveable[2]
+        insert(maze_scheme, moveable)
+      end
+    until not moveable or #potentials == 0
+  end
+
+  local function caveMaze()
+    for _, movement in ipairs(maze_scheme) do
+      local pos1, pos2 = movement[1], movement[1] + movement[2]
+      for x = pos1.x, pos2.x do
+        map.set(x, po1.y, schematics.FLOOR)
+      end
+      for y = pos1.y, pos2.y do
+        map.set(pos1.x, y, schematics.FLOOR)
+      end
+    end
+    return map
+  end
+
+  setStartPoint()
+  generateMaze()
+  return caveMaze()
 end
 
 
