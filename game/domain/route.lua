58345f19b7d1cf39ec7fab141df0822d01c59d3c
Kazuo256 <kazuo256@gmail.com>
2017-08-23 14:56:15 -0300

    Create an IDGenerator from Route class

--- game/domain/route.lua
+++ game/domain/route.lua
@@ -1,29 +1,27 @@
 
 local Route = require 'lux.class' :new{}
-
-local ID_BASE = 1000
-local ID_FORMAT = "#%d"
+local IDGenerator = require 'common.idgenerator'
 
 function Route:instance(obj)
 
-  local _next_id = 1
+  local _id_generator = IDGenerator()
 
   Util.destroyAll 'true_force'
 
   function obj.loadState(state)
-
+    _id_generator = IDGenerator(state.next_id)
   end
 
   function obj.saveState()
     local state = {}
+    state.next_id = _id_generator.getNextID()
     return state
   end
 
   function obj.register(element)
-    local id = ID_FORMAT:format(ID_BASE + _next_id)
+    local id = _id_generator.newID()
     element:setId(id)
     element:setSubtype(element.spectype)
-    _next_id = _next_id + 1
     return id, element
   end
 
