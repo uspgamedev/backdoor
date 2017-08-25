d3e73b77ca438a53e7a1f37667c17e559f6577b5
Kazuo256 <kazuo256@gmail.com>
2017-08-25 14:03:55 -0300

    Add some comments and round up stuff
    + Only change integer input value when it really changes
    + Sort list of options alphabetically

--- game/debug/view/specification_editor.lua
+++ game/debug/view/specification_editor.lua
@@ -5,11 +5,14 @@ local spec_item = {}
 
 function spec_item:integer(spec, domain_name, key)
   local value = spec[key.id]
-  local _, newvalue = imgui.InputInt(key.name, value or 0, 1, 10)
-  spec[key.id] = newvalue
+  local changed, newvalue = imgui.InputInt(key.name, value or 0, 1, 10)
+  if changed then
+    spec[key.id] = newvalue
+  end
 end
 
 function spec_item:enum(spec, domain_name, key)
+  -- Build option list from given array or from a database domain
   local options = key.options
   if type(options) == 'string' then
     local domain = DB.loadDomain(options)
@@ -17,7 +20,9 @@ function spec_item:enum(spec, domain_name, key)
     for k,v in pairs(domain) do
       table.insert(options,k)
     end
+    table.sort(options)
   end
+  -- Find the index of the currently assigned option
   local current = 0
   for i,option in ipairs(options) do
     if option == spec[key.id] then
@@ -25,6 +30,7 @@ function spec_item:enum(spec, domain_name, key)
       break
     end
   end
+  -- Value getter/setter
   local function value(newvalue)
     if newvalue then
       current = newvalue
