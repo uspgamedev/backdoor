3f4f7ac05d80ee724896dbf9ab4a2c66d304fdb3
Kazuo256 <kazuo256@gmail.com>
2017-08-26 17:04:05 -0300

    Load spec input partials from other files

--- game/debug/view/specification_editor.lua
+++ game/debug/view/specification_editor.lua
@@ -3,6 +3,15 @@ local DB = require 'database'
 
 local spec_item = {}
 
+for _,file in ipairs(love.filesystem.getDirectoryItems "debug/view/input") do
+  if file:match "^.+%.lua$" then
+    file = file:gsub("%.lua", "")
+    for k,input in ipairs(require('debug.view.input.' .. file)) do
+      spec_item[k] = input
+    end
+  end
+end
+
 --[[ Invalid input ]]-----------------------------------------------------------
 
 local function _invalid(spec, key)
@@ -123,7 +132,7 @@ return function(spec, domain_name, title, delete)
   local inputs = {}
   for _,key in DB.schemaFor(domain_name) do
     local input = spec_item[key.type]
-    table.insert(inputs, (input or _invalid)(spec, key))
+    table.insert(inputs, (input or _invalid)(spec, key) or _invalid())
   end
 
   return title .. " Editor", 1, function(self)
