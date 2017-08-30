b315cfead79eb8dbca6d017f3b5abf86e2744b40
orenjiakira <orenjiakira@gmail.com>
2017-08-30 16:37:05 -0300

    Add queues to switcher pops and pushes

--- game/infra/switcher.lua
+++ game/infra/switcher.lua
@@ -1,31 +1,41 @@
 
 local Gamestate = require "steaming.extra_libs.hump.gamestate"
+local Queue = require 'lux.common.Queue'
 
 local SWITCHER = {}
 
-local _pushed = false
-local _popped = false
+local _stack_size = 0
+local _pushed = Queue(16)
+local _popped = Queue(16)
 
 function SWITCHER.init()
   Gamestate.registerEvents()
 end
 
+function SWITCHER.switch(to, ...)
+  if _stack_size == 0 then _stack_size = 1 end
+  print("SWITCH", _stack_size)
+  Gamestate.switch(to, ...)
+end
+
 function SWITCHER.push(to, ...)
-  _pushed = { to, ... }
+  _stack_size = _stack_size + 1
+  print("PUSH", _stack_size)
+  _pushed.push { to, ... }
 end
 
 function SWITCHER.pop(...)
-  _popped = { ... }
+  _stack_size = _stack_size - 1
+  print("POP", _stack_size)
+  _popped.push { ... }
 end
 
 function SWITCHER.handleChangedState()
-  if _popped then
-    Gamestate.pop(unpack(_popped))
-    _popped = false
+  while not _popped.isEmpty() do
+    Gamestate.pop(unpack(_popped.pop()))
   end
-  if _pushed then
-    Gamestate.push(unpack(_pushed))
-    _pushed = false
+  while not _pushed.isEmpty() do
+    Gamestate.push(unpack(_pushed.pop()))
   end
 end
 
