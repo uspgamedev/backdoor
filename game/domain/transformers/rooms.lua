52ddf9a6e658d7020f28d8d049fa595a95d56cb7
orenjiakira <orenjiakira@gmail.com>
2017-09-01 15:59:32 -0300

    Fix room transformer schema

--- game/domain/transformers/rooms.lua
+++ game/domain/transformers/rooms.lua
@@ -10,7 +10,7 @@ transformer.schema = {
   { id = 'minw', name = "Minimum Width", type = 'integer', range = {2,32} },
   { id = 'minh', name = "Minimum Height", type = 'integer', range = {2,32} },
   { id = 'maxw', name = "Maximum Width", type = 'integer', range = {2,32} },
-  { id = 'maxw', name = "Maximum Width", type = 'integer', range = {2,32} },
+  { id = 'maxh', name = "Maximum Height", type = 'integer', range = {2,32} },
   { id = 'count', name = "Room Count", type = 'integer', range = {1,32} },
   { id = 'tries', name = "Maximum Tries", type = 'integer', range = {1,1024} },
 }
