8251c1ba14db9f1548d2cad278e32276844ca4ed
Kazuo256 <kazuo256@gmail.com>
2017-09-04 16:54:46 -0300

    Use sections instead of list for sectors

--- game/database/schema/sector.lua
+++ game/database/schema/sector.lua
@@ -1,15 +1,16 @@
 
 return  {
-  { id = 'width', name = "Size Width", type = "integer", range = {8,128} },
-  { id = 'height', name = "Size Height", type = "integer", range = {8,128} },
-  { id = 'margin-width', name = "Margin Width", type = "integer",
-    range = {0,16} },
-  { id = 'margin-height', name = "Margin Height", type = "integer",
-    range = {0,16} },
   { id = 'bootstrap', name = "Base Settings", type = 'section',
     schema = 'transformers.bootstrap', required = true },
   { id = 'rooms', name = "Room Settings", type = 'section',
     schema = 'transformers.rooms' },
-  { id = 'transformers', name = "Transformer", type = 'list' }
+  { id = 'maze', name = "Maze Settings", type = 'section',
+    schema = 'transformers.maze' },
+  { id = 'connections', name = "Connection Settings", type = 'section',
+    schema = 'transformers.connections' },
+  { id = 'deadends', name = "Deadend Settings", type = 'section',
+    schema = 'transformers.deadends' },
+  { id = 'exits', name = "Exit Settings", type = 'section',
+    schema = 'transformers.exits' },
 }
 
