b2a24588ef97000c4626ffcab916a8cc20856aae
Kazuo256 <kazuo256@gmail.com>
2017-09-05 14:23:22 -0300

    Data-drive range of generated encounters

--- game/domain/transformers/encounters.lua
+++ game/domain/transformers/encounters.lua
@@ -5,15 +5,23 @@ local SCHEMATICS  = require 'domain.definitions.schematics'
 local transformer = {}
 
 transformer.schema = {
-  {
-  },
+  { id = 'min', name = "Minimum number of encounters", type = 'integer',
+    range = {1} },
+  { id = 'max', name = "Maximum number of encounters", type = 'integer',
+    range = {1} },
 }
+
+local function _hash(i,j)
+  return ("%s:%s"):format(i,j)
+end
 
 function transformer.process(sectorinfo, params)
   local grid = sectorinfo.grid
   local encounters = {}
+  local total = RANDOM.generate(params.min, params.max)
+  local used = {}
 
-  for i=1,20 do
+  for i=1,total do
     local encounter = {}
     encounter.monster = { 'dumb', 'slime' }
     local minj, maxj, mini, maxi = grid.getRange()
@@ -21,8 +29,9 @@ function transformer.process(sectorinfo, params)
     repeat
       i = RANDOM.generate(mini, maxi)
       j = RANDOM.generate(minj, maxj)
-    until grid.get(j,i) == SCHEMATICS.FLOOR
+    until grid.get(j,i) == SCHEMATICS.FLOOR and not used[_hash(i,j)]
     encounter.pos = {i,j}
+    used[_hash(i,j)] = true
     table.insert(encounters, encounter)
   end
   sectorinfo.encounters = encounters
