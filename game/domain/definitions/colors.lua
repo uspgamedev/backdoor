dcd550d6b18fdf6dc88067e6365c1991aa16eec8
orenjiakira <orenjiakira@gmail.com>
2017-09-08 23:36:39 -0300

    Add colors for reference

--- game/domain/definitions/colors.lua
+++ game/domain/definitions/colors.lua
@@ -1,6 +1,8 @@
 
 local COLORS = {}
 
+COLORS.NEUTRAL = { 0xff, 0xff, 0xff }
+COLORS.BACKGROUND = { 50, 80, 80, 255 }
 COLORS.EXIT = { 0x77, 0xba, 0x99, 0xff }
 COLORS.FLOOR1 = { 25, 73, 95, 0xff }
 COLORS.FLOOR2 = { 25, 73, 95 + 20, 0xff }
