ad5a838340693d28394f4584a7a30b5a95ff3d30
Kazuo256 <kazuo256@gmail.com>
2017-09-13 15:26:23 -0300

    Set default label values for reference fields

--- game/domain/operators/integer_binop.lua
+++ game/domain/operators/integer_binop.lua
@@ -5,7 +5,7 @@ local OP = {}
 OP.schema = {
   { id = 'lhs', name = "Operand 1", type = 'value', match = 'integer' },
   { id = 'op', name = "Operator", type = 'enum',
-  options = {'+', '-', '*', '/'} },
+    options = {'+', '-', '*', '/'} },
   { id = 'rhs', name = "Operand 2", type = 'value', match = 'integer' },
   { id = 'output', name = "Label", type = 'output' }
 }
