533292a46d841ed5b08073f6f4e77b86ed6f564d
rilifon <pudimdefrutas@gmail.com>
2017-09-15 15:10:32 -0300

    Added filter effect for buffer_picker state

--- game/setup.lua
+++ game/setup.lua
@@ -27,11 +27,12 @@ function setup.config()
     --INITIALIZING TABLES--
     --Drawing Tables
     DRAW_TABLE = {
-        BG  = {}, --Background (bottom layer, first to draw)
-        L1  = {}, --Layer 1
-        L2  = {}, --Layer 2
-        HUD = {}, --HUD (top layer, second to last to draw)
-        GUI = {}, --Graphic User Interface (top layer, last to draw)
+        BG    = {}, --Background (bottom layer, first to draw)
+        L1    = {}, --Layer 1
+        L2    = {}, --Layer 2
+        HUDl  = {}, --HUD lower (drawn before HUD)
+        HUD   = {}, --HUD (top layer, second to last to draw)
+        GUI   = {}, --Graphic User Interface (top layer, last to draw)
     }
     --Other Tables
     SUBTP_TABLE = {} --Table with tables for each subtype (for fast lookup)
