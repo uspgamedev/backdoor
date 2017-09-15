8e35d69a8b78cb607ea49dc62587fa4dd44c2924
rilifon <pudimdefrutas@gmail.com>
2017-09-15 14:33:03 -0300

    Added a card back image and improved buffer_picker state

--- game/domain/view/bufferpickerview.lua
+++ game/domain/view/bufferpickerview.lua
@@ -19,14 +19,24 @@ function BufferPickerView:init(actor)
 end
 
 function BufferPickerView:draw()
-  local X,Y = O_WIN_W/2, O_WIN_H/2
+  local c_x, c_y = O_WIN_W/2, O_WIN_H/2 --Center of screen position
   local g = love.graphics
   local w,h = 128,128
   local size = self.actor:getBufferSize(self.select)
-  g.setColor(100, 100, 150)
-  g.rectangle('fill', X-w/2, Y-h/2, w, h)
+  local img = IMG.CARD_BACK_1
+  local i_w, i_h = img:getWidth(), img:getHeight() -- Image width and height
+  local i_s = 1 --Image scale
+  local i_x, i_y = c_x-i_w/2*i_s, c_y-i_h/2*i_s --Image position
+  local i_r = 0 --Image rotation
+  g.setColor(255, 255, 255)
+  for i = 1, math.floor(size/2) do
+    g.draw(img, i_x, i_y, i_r, i_s) --Draw image
+    i_x, i_y = i_x + 10, i_y - 10
+  end
+
+  --Draw buffer number and remaining cards
   g.setColor(255, 255, 200)
-  g.print(("%d (%2d)"):format(self.select, size), X, Y)
+  g.print(("%d (%2d)"):format(self.select, size), c_x, c_y + i_h/2 + 40)
 end
 
 function BufferPickerView:moveSelection(dir)
@@ -45,4 +55,3 @@ function BufferPickerView:getSelection()
 end
 
 return BufferPickerView
-
--- game/setup.lua
+++ game/setup.lua
@@ -41,7 +41,9 @@ function setup.config()
     CAM = Camera(O_WIN_W/2, O_WIN_H/2) --Set camera position to center of screen
 
     --IMAGES--
+    local path = "assets/imgs/"
     IMG = { --Table containing all the images
+      CARD_BACK_1 = love.graphics.newImage(path.."cards/card_back_1.png")
     }
 
     --AUDIO--
