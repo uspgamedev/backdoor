f01b7231d6fbe09c6331bf33e83529557077515f
Kazuo256 <kazuo256@gmail.com>
2017-09-18 15:44:39 -0300

    Refactor common code

--- game/debug/view/input/common.lua
+++ game/debug/view/input/common.lua
@@ -3,59 +3,54 @@ local IMGUI = require 'imgui'
 
 local inputs = {}
 
-function inputs.boolean(spec, key)
-  return function (self)
-    IMGUI.Text(key.name)
-    local value = spec[key.id] or false
-    IMGUI.PushID(key.id)
-    local changed, newvalue = IMGUI.Checkbox("", value)
-    IMGUI.PopID()
-    if changed then
-      spec[key.id] = newvalue
+local function _makeCommon(default, call)
+  return function(spec, key)
+    return function(self)
+      IMGUI.Text(key.name)
+      local value = spec[key.id] or default
+      IMGUI.PushID(key.id)
+      local changed, newvalue = call(value, key)
+      IMGUI.PopID()
+      if changed then
+        spec[key.id] = newvalue
+      end
     end
   end
 end
 
-function inputs.integer(spec, key)
-  local inputInt = require 'debug.view.helpers.integer'
-  return function (self)
-    IMGUI.Text(key.name)
-    local value = spec[key.id] or (key.range or {0})[1]
-    IMGUI.PushID(key.id)
-    local changed, newvalue = inputInt(value, "", range)
-    IMGUI.PopID()
-    if changed then
-      spec[key.id] = newvalue
-    end
+inputs.boolean = _makeCommon(
+  false,
+  function(value, key)
+    return IMGUI.Checkbox("", value)
   end
-end
+)
 
-function inputs.string(spec, key)
-  local inputStr = require 'debug.view.helpers.string'
-  return function (self)
-    IMGUI.Text(key.name)
-    local value = spec[key.id] or ""
-    IMGUI.PushID(key.id)
-    local changed, newvalue = inputStr(value, "")
-    IMGUI.PopID()
-    if changed then
-      spec[key.id] = newvalue
+inputs.integer = _makeCommon(
+  nil,
+  function(value, key)
+    local range = key.range
+    local changed, newvalue = IMGUI.InputInt("", value, 1, 10)
+    if range then
+      newvalue = math.max(range[1],
+                          range[2] and math.min(range[2], newvalue) or newvalue)
     end
+    return changed, newvalue
   end
-end
+)
 
-function inputs.text(spec, key)
-  return function (self)
-    IMGUI.Text(key.name)
-    local value = spec[key.id] or ""
-    IMGUI.PushID(key.id)
-    local changed, newvalue = IMGUI.InputTextMultiline("", value, 256,
-                                                       208,100)
-    IMGUI.PopID()
-    if changed then
-      spec[key.id] = newvalue
-    end
+inputs.string = _makeCommon(
+  "",
+  function(value, key)
+    return IMGUI.InputText("", value, 64)
   end
-end
+)
+
+inputs.text = _makeCommon(
+  "",
+  function(value, key)
+    return IMGUI.InputTextMultiline("", value, 256)
+  end
+)
 
 return inputs
+
