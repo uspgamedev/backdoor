c3bd0c44968360d7688fa6a05dd314e407f9d54c
orenjiakira <orenjiakira@gmail.com>
2017-09-19 17:30:54 -0300

    Add fillings transformer

---
+++ game/domain/transformers/fillings.lua
@@ -0,0 +1,50 @@
+
+local SCHEMATICS = require 'domain.definitions.schematics'
+
+local transformer = {}
+
+transformer.schema = {}
+
+function transformer.process(sectorinfo, params)
+  local sectorgrid = sectorinfo.grid
+  local potentials, n
+
+  local function _findPotentials()
+    potentials = potentials or {}
+    n = 0
+    for x, y, tile in sectorgrid.iterate() do
+      if sectorgrid.isInsideMargins(x, y)
+        and sectorgrid.get(x, y) == SCHEMATICS.NAUGHT then
+        local count = 0
+        for j = x-1, x+1 do
+          for i = y-1, y+1 do
+            if not (j == x and y == i) then
+              if sectorgrid.get(j, i) == SCHEMATICS.NAUGHT then
+                count = count + 1
+              end
+            end
+          end
+        end
+        if count <= 1 then
+          n = n + 1
+          potentials[n] = {x, y}
+        end
+      end
+    end
+  end
+
+  repeat
+    _findPotentials()
+    if n > 0 then
+      for idx = 1, n do
+        local x, y = unpack(potentials[idx])
+        sectorgrid.set(x, y, SCHEMATICS.FLOOR)
+      end
+    end
+  until n == 0
+
+  return sectorinfo
+end
+
+return transformer
+
