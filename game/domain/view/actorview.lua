206987a6dba6a7298502b68bee126d438f9dd144
orenjiakira <orenjiakira@gmail.com>
2017-09-21 15:46:29 -0300

    Add actor stats to actorview

--- game/domain/view/actorview.lua
+++ game/domain/view/actorview.lua
@@ -7,13 +7,15 @@ local ActorView = Class{
 }
 
 local _initialized = false
-local WIDTH, HEIGHT
-local EXPTEXT
+local _exptext, _statstext
+local WIDTH, HEIGHT, FONT
 
 local function _initGraphicValues()
   WIDTH, HEIGHT = love.graphics.getDimensions()
   FONT = love.graphics.newFont(DB.loadFontPath("Saira"), 24)
-  EXPTEXT = "EXP: %d"
+  FONT:setLineHeight(1)
+  _exptext = "EXP: %d"
+  _statstext = "STATS\nATH: %d\nARC: %d\nMEC: %d"
   _initialized = true
 end
 
@@ -30,11 +32,21 @@ end
 function ActorView:draw()
   local g = love.graphics
   local actor = self.route.getControlledActor()
+  local ath = actor:getATH()
+  local arc = actor:getARC()
+  local mec = actor:getMEC()
+
   g.push()
+
   g.setFont(FONT)
   g.setColor(COLORS.NEUTRAL)
+
   g.translate(40, 40)
-  g.print(EXPTEXT:format(actor:getExp()), 0, 0)
+  g.print(_exptext:format(actor:getExp()), 0, 0)
+
+  g.translate(0, 1.5*FONT:getHeight())
+  g.print(_statstext:format(ath, arc, mec))
+
   g.pop()
 end
 
