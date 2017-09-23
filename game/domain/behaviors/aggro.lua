5c1b3c35d63716b73cec57340e1d67e1bd480090
orenjiakira <orenjiakira@gmail.com>
2017-09-23 02:08:37 -0300

    Add slightly inefficient pathfinding

--- game/domain/behaviors/aggro.lua
+++ game/domain/behaviors/aggro.lua
@@ -3,6 +3,53 @@ local DIR = require 'domain.definitions.dir'
 local Action = require 'domain.action'
 local TILE = require 'common.tile'
 
+local function _hash(pos)
+  if not pos then return "none" end
+  return string.format("%d:%d", unpack(pos))
+end
+
+local function get_path(start, goal, sector)
+  local frontier, head = {start}, 1
+  local came_from = {}
+  local path = {}
+  local found = false
+
+  came_from[_hash(start)] = start
+
+  while head <= #frontier do
+    local current = frontier[head]
+    head = head + 1
+
+    if TILE.distUniform(goal[1], goal[2], unpack(current)) == 1 then
+      found = true
+      goal = current
+      break
+    end
+
+    for n = 1, 4 do
+      local i, j = unpack(current)
+      local di, dj = unpack(DIR[DIR[n]])
+      local next_pos = { i+di, j+dj }
+
+      if not came_from[_hash(next_pos)]
+        and sector:isValid(unpack(next_pos)) then
+        came_from[_hash(next_pos)] = current
+        table.insert(frontier, next_pos)
+      end
+    end
+  end
+
+  local current = goal
+  if found then
+    while _hash(start) ~= _hash(current) do
+      table.insert(path, current)
+      current = came_from[_hash(current)]
+    end
+    return path[#path]
+  end
+  return false
+end
+
 return function (actor, sector)
   local actorlist = sector:getActors()
   local target, dist
@@ -23,21 +70,11 @@ return function (actor, sector)
   if dist == 1 then
     -- attack if close!
     return 'PRIMARY', { target = {target:getPos()} }
-  elseif dist <= 4 then
+  elseif dist <= 5 then
     -- chase if far away!
-    local ni, nj = i, j
-    for n = 1, 4 do
-      local di, dj = unpack(DIR[DIR[n]])
-      local dist = TILE.distUniform(i, j, target:getPos())
-      local newdist = TILE.distUniform(i+di, j+dj, target:getPos())
-      if newdist < dist then
-        ni = i + di
-        nj = j + dj
-      end
-    end
-
-    if sector:isValid(ni, nj) and (i ~= ni or j ~= nj) then
-      return 'MOVE', { pos = {ni, nj} }
+    local pos = get_path({i,j}, {target:getPos()}, sector)
+    if pos then
+      return 'MOVE', { pos = pos }
     end
   end
 
