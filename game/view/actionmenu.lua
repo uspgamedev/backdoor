e8d16256c7a22cf05c3af1585e8667b04ec902fd
Kazuo256 <kazuo256@gmail.com>
2017-10-06 17:29:32 -0300

    Add tween for changing menu focus

--- game/view/actionmenu.lua
+++ game/view/actionmenu.lua
@@ -25,6 +25,8 @@ function ActionMenu:init()
   ELEMENT.init(self)
   self.current = 1
   self.enter = 0
+  self.switch = 0
+  self.tween = nil
   _W, _H = love.graphics.getDimensions()
 
 end
@@ -32,9 +34,18 @@ end
 function ActionMenu:moveFocus(dir)
   if dir == "up" then
     self.current = math.max(1, self.current - 1)
+    self.switch = 1
   elseif dir == "down" then
     self.current = math.min(#_ACTIONS, self.current + 1)
+    self.switch = -1
+  else
+    return
   end
+  if self.tween then
+    MAIN_TIMER:cancel(self.tween)
+  end
+  self.tween = MAIN_TIMER:tween(0.3, self, { switch = 0 }, 'out-back',
+                                function () self.tween = nil end)
 end
 
 function ActionMenu:getSelected()
@@ -42,57 +53,28 @@ function ActionMenu:getSelected()
 end
 
 function ActionMenu:open()
-  --if self.fadeout then
-  --  MAIN_TIMER:cancel(self.fadeout)
-  --  self.fadeout = false
-  --  self.invisible = true
-  --end
-  --if self.fadein or not self.invisible then
-  --  return
-  --end
-  --self.selected = false
-  --self.invisible = false
-  --self.enter = self.enter or { 0 }
-  MAIN_TIMER:tween(0.1, self, { enter = 1 }, 'in-circ')
+  MAIN_TIMER:tween(0.2, self, { enter = 1 }, 'out-circ')
 end
 
 function ActionMenu:close(after)
-  MAIN_TIMER:tween(0.1, self, { enter = 0 }, 'out-circ', after)
-  --if self.fadein then
-  --  MAIN_TIMER:cancel(self.fadein)
-  --  self.fadein = false
-  --end
-  --if self.fadeout or self.invisible then
-  --  return
-  --end
-  --self.enter = self.enter or { 1 }
-  --self.fadeout = MAIN_TIMER:tween(
-  --  0.5, self.enter, { 0 }, 'out-cubic',
-  --  function ()
-  --    self.fadeout = false
-  --    self.invisible = true
-  --  end
-  --)
+  MAIN_TIMER:tween(0.2, self, { enter = 0 }, 'out-circ', after)
 end
 
 function ActionMenu:draw()
   local g = love.graphics
   local enter = self.enter
+  local switch = self.switch
   local cos, sin, pi = math.cos, math.sin, math.pi
   CAM:zoomTo(1 + enter)
   g.push()
   g.translate(_W/2, _H/2 - 40)
-  local rot = 0
+  local rot = (enter - 1) * pi + switch * _ANGLE
   for i,action_name in ipairs(_ACTIONS) do
     local k = i - self.current
     g.push()
     local x,y = cos(rot - _ANGLE*k), -sin(rot - _ANGLE*k)
     g.translate(_RADIUS*x, _RADIUS*y)
-    --if self.selected == i+1 then
-    --  g.scale(1.5, 1.5)
-    --end
-    --rgb(229, 181, 59)
-    local fade = (i == self.current) and 1 or 0.5
+    local fade = (i == self.current) and (1 - math.abs(switch)/2) or 0.5
     g.setColor(80, 10, 50, enter*fade*100)
     g.circle("fill", 8, 8, 64*fade)
     g.setColor(230, 180, 60, enter*fade*255)
