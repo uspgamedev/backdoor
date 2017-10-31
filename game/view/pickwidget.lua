bf26a4ef57487ed35f42f3a4c41812f55bc0102a
orenjiakira <orenjiakira@gmail.com>
2017-10-31 01:17:49 -0200

    Add widget state info on widget view

--- game/view/pickwidget.lua
+++ game/view/pickwidget.lua
@@ -13,18 +13,21 @@ local _FONT_SIZE = 24
 local _BLOCK_HEIGHT = 48
 local _MARGIN = 16
 local _PADDING = 4
-local _SLOTS = {'A', 'B', 'C', 'D'}
-local _FMT = "SLOT %s: %s"
+local _FMT = "%s [%d/%d%s]"
+local _TRIGGERS = {
+  [DEFS.TRIGGERS.ON_USE] = " uses",
+  [DEFS.TRIGGERS.ON_TURN] = " turns",
+  [DEFS.TRIGGERS.ON_TICK] = " ticks",
+}
 
 local _width, _height
-local _font, _text_width, _pd
+local _font
 local _alpha
 
 local function _initGraphicValues()
   local g = love.graphics
   _width, _height = g.getDimensions()
   _font = _font or FONT.get(_FONT_NAME, _FONT_SIZE)
-  _text_width = _width/4
 end
 
 function PickWidgetView:init(target_actor)
@@ -49,23 +52,32 @@ function PickWidgetView:draw()
 
   g.translate(_width/8, _height/2-2*(_BLOCK_HEIGHT+_MARGIN))
   -- draw stuff
+  local strs = {}
+  local width = 0
   for index, widget in self.target:getBody():eachWidget() do
-    local name, pd
-    name = widget:getName()
+    print(widget:getWidgetTrigger())
+    strs[index] = _FMT:format(widget:getName(),
+                              widget:getWidgetCharges() - widget:getUsages(),
+                              widget:getWidgetCharges(),
+                              _TRIGGERS[widget:getWidgetTrigger()] or ""
+    )
+    width = math.max(width, _font:getWidth(strs[index]))
+  end
+  for index, info_str in ipairs(strs) do
     local selected = self.selection == index
     if selected then
       g.setColor(0xff, 0xff, 0xff, self.alpha*0xff)
     else
       g.setColor(0x16, 0x16, 0x16, self.alpha*0xff)
     end
-    g.rectangle("fill", 0, 0, _text_width, _BLOCK_HEIGHT)
+    g.rectangle("fill", 0, 0, width+8*_PADDING, _BLOCK_HEIGHT)
     _font:set()
     if selected then
       g.setColor(0x00, 0x00, 0x00, self.alpha*0xff)
     else
       g.setColor(0xff, 0xff, 0xff, self.alpha*0xff)
     end
-    g.print(_FMT:format(_SLOTS[index], name), 4*_PADDING, _PADDING)
+    g.printf(info_str, 4*_PADDING, _PADDING, width)
     g.translate(0, _BLOCK_HEIGHT + _MARGIN)
   end
 
