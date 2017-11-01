553619c4a1f5240dc1167d475bb324a18e6eec0e
Kazuo256 <kazuo256@gmail.com>
2017-11-01 11:53:26 -0200

    Remove stash bonus cards from initial deck

--- game/infra/routebuilder.lua
+++ game/infra/routebuilder.lua
@@ -17,9 +17,6 @@ end
 local function _simpleBuffer()
   local buffer = {}
   for i=1,2 do
-    table.insert(buffer, _card('arc+'))
-    table.insert(buffer, _card('mec+'))
-    table.insert(buffer, _card('ath+'))
     table.insert(buffer, _card('bolt'))
     table.insert(buffer, _card('draw'))
     table.insert(buffer, _card('cure'))
