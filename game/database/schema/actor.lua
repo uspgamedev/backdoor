4e6dcabaa726fc57b4a32d335b3829eb15870619
orenjiakira <orenjiakira@gmail.com>
2017-11-09 19:25:07 -0200

    Add initial buffers to DB

--- game/database/schema/actor.lua
+++ game/database/schema/actor.lua
@@ -10,6 +10,7 @@ return {
   { id = 'extends', name = "Prototype", type = 'enum', options = 'domains.actor',
     optional = true },
   { id = 'name', name = "Full Name", type = 'string' },
+  { id = 'description', name = "Description", type = 'text' },
   { id = 'behavior', name = "Behavior", type = 'enum',
     options = behaviors },
   { id = 'signature', name = "Signature Ability", type = 'enum',
@@ -18,7 +19,12 @@ return {
   { id = 'arc', name = "ARC", type = 'integer', range = {0} },
   { id = 'mec', name = "MEC", type = 'integer', range = {0} },
   { id = 'spd', name = "SPD", type = 'integer', range = {0} },
-  { id = 'collection', name = "Card Collection", type = 'enum',
+  { id = 'collection', name = "Drops", type = 'enum',
     options = 'domains.collection' },
-  { id = 'description', name = "Description", type = 'text' },
+  { id = 'initial_buffer', name = "Buffer Card", type = 'array',
+    schema = {
+      { id = 'card', name = "Card", type = 'enum', options = "domains.card" },
+      { id = 'amount', name = "Amount", type = 'integer', range = {1,16} },
+    }
+  },
 }
