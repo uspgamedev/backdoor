f53cf688ad6da5154d74a7abea02ce0ced32dd34
orenjiakira <orenjiakira@gmail.com>
2017-12-11 00:50:25 -0200

    Refactor user turn card select and animation

--- game/gamestates/card_select.lua
+++ game/gamestates/card_select.lua
@@ -1,6 +1,6 @@
 --MODULE FOR THE GAMESTATE: SELECTING A CARD IN HAND--
+local INPUT = require 'input'
 
-local CONTROL = require 'infra.control'
 
 local state = {}
 
@@ -9,27 +9,6 @@ local state = {}
 
 local _route
 local _hand_view
-
-local _task
-
-local _mapped_signals
-local _previous_control_map
-
-local SIGNALS = {
-  PRESS_RIGHT = {"move_focus", "right"},
-  PRESS_LEFT = {"move_focus", "left"},
-  PRESS_UP = {"change_action_type", "up"},
-  PRESS_DOWN = {"change_action_type", "down"},
-  PRESS_CONFIRM = {"confirm"},
-  PRESS_CANCEL = {"cancel"},
-  PRESS_SPECIAL = {"cancel"},
-  PRESS_PAUSE = {"pause"},
-  PRESS_QUIT = {"quit"}
-}
-
---LOCAL FUNCTIONS DECLARATIONS
-local _unregisterSignals
-local _registerSignals
 
 --LOCAL FUNCTIONS--
 
@@ -57,43 +36,16 @@ local function _cancel()
   SWITCHER.pop(args)
 end
 
-function _registerSignals()
-  Signal.register("move_focus", _moveFocus)
-  Signal.register("change_action_type", _changeActionType)
-  Signal.register("confirm", _confirmCard)
-  Signal.register("cancel", _cancel)
-  CONTROL.setMap(_mapped_signals)
-end
-
-function _unregisterSignals()
-  for _,signal_pack in pairs(SIGNALS) do
-    Signal.clear(signal_pack[1])
-  end
-  CONTROL.setMap(_previous_control_map)
-end
-
 --STATE FUNCTIONS--
 
 function state:init()
-  _mapped_signals = {}
-  for input_name, signal_pack in pairs(SIGNALS) do
-    _mapped_signals[input_name] = function ()
-      Signal.emit(unpack(signal_pack))
-    end
-  end
 end
 
 function state:enter(_, route, hand_view)
 
   _route = route
   _hand_view = hand_view
-
   _hand_view:activate()
-
-  _registerSignals()
-
-  _previous_control_map = CONTROL.getMap()
-  CONTROL.setMap(_mapped_signals)
 
   --Make cool animation for cards showing up
 
@@ -103,14 +55,27 @@ function state:leave()
 
   _hand_view:deactivate()
 
-  _unregisterSignals()
-
 end
 
 function state:update(dt)
 
   if not DEBUG then
     MAIN_TIMER:update(dt)
+  end
+
+  if INPUT.wasActionPressed('RIGHT') then
+    _moveFocus("right")
+  elseif INPUT.wasActionPressed('LEFT') then
+    _moveFocus("left")
+  elseif INPUT.wasActionPressed('UP') then
+    _changeActionType("up")
+  elseif INPUT.wasActionPressed('DOWN') then
+    _changeActionType("down")
+  elseif INPUT.wasActionPressed('CONFIRM') then
+    _confirmCard()
+  elseif INPUT.wasActionPressed('CANCEL') or
+         INPUT.wasActionPressed('SPECIAL') then
+    _cancel()
   end
 
   Util.destroyAll()
@@ -121,48 +86,6 @@ function state:draw()
 
     Draw.allTables()
 
-end
-
-function state:keypressed(key)
-
-  imgui.KeyPressed(key)
-  if imgui.GetWantCaptureKeyboard() then
-     return
-  end
-
-  if key ~= "escape" then
-      Util.defaultKeyPressed(key)
-  end
-
-end
-
-function state:textinput(t)
-  imgui.TextInput(t)
-end
-
-function state:keyreleased(key)
-
-    imgui.KeyReleased(key)
-    if imgui.GetWantCaptureKeyboard() then
-       return
-    end
-
-end
-
-function state:mousemoved(x, y)
-  imgui.MouseMoved(x, y)
-end
-
-function state:mousepressed(x, y, button)
-  imgui.MousePressed(button)
-end
-
-function state:mousereleased(x, y, button)
-  imgui.MouseReleased(button)
-end
-
-function state:wheelmoved(x, y)
-  imgui.WheelMoved(y)
 end
 
 --Return state functions
