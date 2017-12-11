ba415d2f0a2d396424c839f977dd09a9ee3db764
orenjiakira <orenjiakira@gmail.com>
2017-12-11 00:15:53 -0200

    Refactor action menu to use new input method

--- game/gamestates/action_menu.lua
+++ game/gamestates/action_menu.lua
@@ -1,8 +1,8 @@
 
 --- GAMESTATE: Choosing an action
 
-local CONTROL         = require 'infra.control'
-local ActionMenuView  = require 'view.actionmenu'
+local INPUT          = require 'input'
+local ActionMenuView = require 'view.actionmenu'
 
 local state = {}
 
@@ -11,9 +11,6 @@ local state = {}
 local _menu_view
 local _last_focus
 
-local _mapped_signals
-local _previous_control_map
-
 local SIGNALS = {
   PRESS_UP = {"move_focus", "up"},
   PRESS_DOWN = {"move_focus", "down"},
@@ -21,11 +18,6 @@ local SIGNALS = {
   PRESS_CANCEL = {"cancel"},
   PRESS_EXTRA = {"cancel"},
 }
-
---[[ LOCAL FUNCTIONS DECLARATIONS ]]--
-
-local _unregisterSignals
-local _registerSignals
 
 --[[ LOCAL FUNCTIONS ]]--
 
@@ -45,30 +37,9 @@ local function _cancel()
   SWITCHER.pop({})
 end
 
-function _registerSignals()
-  Signal.register("move_focus", _moveFocus)
-  Signal.register("confirm", _confirm)
-  Signal.register("cancel", _cancel)
-  _previous_control_map = CONTROL.getMap()
-  CONTROL.setMap(_mapped_signals)
-end
-
-function _unregisterSignals()
-  for _,signal_pack in pairs(SIGNALS) do
-    Signal.clear(signal_pack[1])
-  end
-  CONTROL.setMap(_previous_control_map)
-end
-
 --[[ STATE FUNCTIONS ]]--
 
 function state:init()
-  _mapped_signals = {}
-  for input_name, signal_pack in pairs(SIGNALS) do
-    _mapped_signals[input_name] = function ()
-      Signal.emit(unpack(signal_pack))
-    end
-  end
   _menu_view = ActionMenuView()
   _menu_view:addElement('HUD')
 end
@@ -80,20 +51,27 @@ function state:enter(_, route)
   if player:getHandSize() > 0 then action = 'play_card' end
   _menu_view:setCardAction(action)
   _menu_view:open(_last_focus, player)
-  _registerSignals()
 
 end
 
 function state:leave()
-
-  _unregisterSignals()
-
 end
 
 function state:update(dt)
 
   if not DEBUG then
     MAIN_TIMER:update(dt)
+  end
+
+  if INPUT.wasActionPressed('UP') then
+    _moveFocus('up')
+  elseif INPUT.wasActionPressed('DOWN') then
+    _moveFocus('down')
+  elseif INPUT.wasActionPressed('CONFIRM') then
+    _confirm()
+  elseif INPUT.wasActionPressed('CANCEL') or
+    _cancel()
+         INPUT.wasActionPressed('EXTRA') then
   end
 
 end
