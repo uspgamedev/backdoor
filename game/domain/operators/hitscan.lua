87dbec1712151a47e8c61b765d9e62b0b5b62d07
Kazuo256 <kazuo256@gmail.com>
2017-12-14 14:37:12 -0200

    Add max range for hitscan
    And apply to Rush

--- game/domain/operators/hitscan.lua
+++ game/domain/operators/hitscan.lua
@@ -6,6 +6,8 @@ local OP = {}
 OP.schema = {
   { id = 'pos', name = "Origin", type = 'value', match = 'pos' },
   { id = 'dir', name = "Raycast direction", type = 'value', match = 'dir' },
+  { id = 'maxrange', name = "Maximum range", type = 'value', match = 'integer',
+    range = {1} },
   { id = 'output', name = "Label", type = 'output' }
 }
 
@@ -16,10 +18,13 @@ function OP.process(actor, params)
   local pos = {}
   local next_pos = { unpack(params['pos']) } -- Clone it!
   local dir = params['dir']
+  local maxrange = params['maxrange']
+  local i = 0
   repeat
     pos[1], pos[2] = unpack(next_pos)
     next_pos[1], next_pos[2] = pos[1]+dir[1], pos[2]+dir[2]
-  until not sector:isValid(unpack(next_pos))
+    i = i + 1
+  until i > maxrange or not sector:isValid(unpack(next_pos))
   return pos
 end
 
