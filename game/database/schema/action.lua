839ed9eb0cbf05498ffdce684797a1db1ccf95f2
orenjiakira <orenjiakira@gmail.com>
2017-12-21 16:55:07 -0200

    Rename and change signatures

--- game/database/schema/action.lua
+++ game/database/schema/action.lua
@@ -4,8 +4,5 @@ return {
   { id = 'playpoints', name = "Play Points Reward", type = 'integer',
     range = {0,128} },
   { id = 'ability', name = "Activated Ability", type = 'ability' },
-  { id = 'params', name = "Parameter", type = 'list' },
-  { id = 'operators', name = "Value", type = 'list' },
-  { id = 'effects', name = "Effect", type = 'list' }
 }
 
