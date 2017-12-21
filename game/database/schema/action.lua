711aa9b5359c4696bfee2a55b45615c0a1419741
orenjiakira <orenjiakira@gmail.com>
2017-12-21 19:48:10 -0200

    Remove PP from schemas

--- game/database/schema/action.lua
+++ game/database/schema/action.lua
@@ -1,8 +1,6 @@
 
 return {
   { id = 'cost', name = "Time cost", type = 'integer', range = {0,128} },
-  { id = 'playpoints', name = "Play Points Reward", type = 'integer',
-    range = {0,128} },
   { id = 'ability', name = "Activated Ability", type = 'ability' },
   { id = 'params', name = "Parameter", type = 'list' },
   { id = 'operators', name = "Value", type = 'list' },
