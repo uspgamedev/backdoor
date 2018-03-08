3902cc1c892208be378ed6108ee5576bc21ac2fd
rilifon <pudimdefrutas@gmail.com>
2018-03-08 15:23:07 -0300

    Sbrubbles :wink:

--- game/view/spritefx/dmg_taken.lua
+++ game/view/spritefx/dmg_taken.lua
@@ -9,10 +9,6 @@ local _TILE_W = VIEWDEFS.TILE_W
 local _TILE_H = VIEWDEFS.TILE_H
 
 function SPRITEFX.apply(sectorview, args)
-  --Play sfx if any
-  local sfx
-  if args.sfx then sfx = RES.loadSFX(args.sfx); sfx:setVolume(1); sfx:stop(); sfx:play(); end
-
   local body, amount = args.body, args.amount
   local i, j = body:getPos()
   local body_sprite = sectorview:getBodySprite(body)
