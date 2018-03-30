24b112683a4ddd11bc6df0f9b527b5d9846607ae
orenjiakira <orenjiakira@gmail.com>
2018-03-29 22:17:11 -0300

    Enhance errors in fadeview

--- game/view/fade.lua
+++ game/view/fade.lua
@@ -5,11 +5,15 @@ local FadeView = Class{
 
 local _FADE_TIME = 0.25
 local _FADE_TWEEN = "FADING_TWEEN"
+local _FADE_CONFLICT_ERR = [=[
+Fade animations conflicting.
+Please wait until one of them is finished]=]
 
 --[[ PUBLIC METHODS ]]--
 
 FadeView.STATE_FADED = 1
 FadeView.STATE_UNFADED = 0
+FadeView.FADE_TIME = _FADE_TIME
 
 function FadeView:init(fade_state)
   ELEMENT.init(self)
@@ -20,26 +24,26 @@ function FadeView:init(fade_state)
 end
 
 function FadeView:fadeOutAndThen(do_a_thing)
-  assert(not self.fading_out and not self.fading_in)
+  assert(not self.fading_out and not self.fading_in, _FADE_CONFLICT_ERR)
   self.fading_out = true
   self:removeTimer(_FADE_TWEEN, MAIN_TIMER)
   self:addTimer(_FADE_TWEEN, MAIN_TIMER, "tween", _FADE_TIME,
                 self, { alpha = 1 }, "linear",
                 function()
-                  do_a_thing()
                   self.fading_out = false
+                  do_a_thing()
                 end)
 end
 
 function FadeView:fadeInAndThen(do_a_thing)
-  assert(not self.fading_out and not self.fading_in)
+  assert(not self.fading_out and not self.fading_in, _FADE_CONFLICT_ERR)
   self.fading_in = true
   self:removeTimer(_FADE_TWEEN, MAIN_TIMER)
   self:addTimer(_FADE_TWEEN, MAIN_TIMER, "tween", _FADE_TIME,
                 self, { alpha = 0 }, "linear",
                 function()
-                  do_a_thing()
                   self.fading_in = false
+                  do_a_thing()
                 end)
 end
 
