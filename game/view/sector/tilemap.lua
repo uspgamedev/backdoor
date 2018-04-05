3d0bd5514da68f8002043e7e378c2f7a5666c5be
orenjiakira <orenjiakira@gmail.com>
2018-04-05 12:56:14 -0300

    Remove unimplemented method in sector tilemap helper

--- game/view/sector/tilemap.lua
+++ game/view/sector/tilemap.lua
@@ -81,32 +81,7 @@ function TileMap.drawFloor(g, fov)
 end
 
 function TileMap.drawWallInLine(g, i, fov)
-  _tile_batch:clear()
-  for j = 0, _sector.w-1 do
-    local ti, tj = i+1, j+1
-    local tile = _sector.tiles[ti][tj]
-    if CAM:isTileInFrame(i, j) and tile then
-      local tile_tipe = tile.type
-      if tile_type == SCHEMATICS.WALL then
-        local x, y = j*_TILE_W, i*_TILE_H
-        if fov and fov[ti] then
-          local visibility = fov[ti][tj]
-          if not visibility then
-            _tile_batch:setColor(COLORS.BLACK)
-          elseif visibility == 0 then
-            _tile_batch:setColor(COLORS.HALF_VISIBLE)
-          else
-            _tile_batch:setColor(COLORS.NEUTRAL)
-          end
-          _tile_batch:add(_tile_quads[tile_type], x, y,
-                          0, 1, 1, unpack(_tile_offset[tile_type]))
-        end
-      end
-    end
-  end
-  g.setColor(COLORS.NEUTRAL)
-  g.draw(_tile_batch, 0, 0)
-  _tile_batch:clear()
+  -- to be implemented in v11.0
 end
 
 return TileMap
