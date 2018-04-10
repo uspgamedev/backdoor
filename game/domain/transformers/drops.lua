551336198b226d3236508439980d8a70ce8e9711
orenjiakira <orenjiakira@gmail.com>
2018-04-09 21:24:43 -0300

    Add transformer for drops

---
+++ game/domain/transformers/drops.lua
@@ -0,0 +1,33 @@
+
+local RANDOM = require 'common.random'
+local SCHEMATICS = require 'domain.definitions.schematics'
+
+local _RATE_MAX = 100
+local _FOOD = 'food'
+
+local transformer = {}
+
+transformer.schema = {
+  { id = 'droprate', name = "Food Drop Rate",
+    type = 'integer', range = {0, _RATE_MAX} },
+}
+
+function transformer.process(sectorinfo, params)
+  local grid = sectorinfo.grid
+  local droprate = params.droprate
+
+  local drops = {}
+  for i, j, tile in grid.iterate() do
+    drops[i] = drops[i] or {}
+    drops[i][j] = {}
+    if tile == SCHEMATICS.FLOOR and RANDOM.generate(_RATE_MAX) <= droprate then
+      table.insert(drops[i][j], _FOOD)
+    end
+  end
+
+  sectorinfo.drops = drops
+  return sectorinfo
+end
+
+return transformer
+
