e91e09baa0c2e6a66d65a9a68ddde9da53fb7283
Kazuo256 <kazuo256@gmail.com>
2018-04-12 18:06:27 -0300

    Target finders may ignore same faction
    Signed off by: @rilifon
    
    Close #608

--- game/domain/inputs/has_nearby_bodies.lua
+++ game/domain/inputs/has_nearby_bodies.lua
@@ -11,13 +11,19 @@ INPUT.schema = {
   { id = 'body-type', name = "Body Type", type = 'enum',
     options = 'domains.body' },
   { id = 'ignore-owner', name = "Ignore Owner", type = 'boolean' },
+  { id = 'ignore-same-faction', name = "Ignore Same Faction",
+    type = 'boolean' },
   { id = 'output', name = "Label", type = 'output' }
 }
 
 INPUT.type = 'none'
 
 local function _checkOwner(actor, body, ignore)
-  return not ignore or actor:getBody(body) ~= body
+  return not ignore or actor:getBody() ~= body
+end
+
+local function _checkFaction(actor, body, ignore)
+  return not ignore or actor:getBody():getFaction() ~= body:getFaction()
 end
 
 function INPUT.isValid(actor, fieldvalues, value)
@@ -26,13 +32,15 @@ function INPUT.isValid(actor, fieldvalues, value)
   local range = fieldvalues['range']
   local specname = fieldvalues['body-type']
   local notowner = fieldvalues['ignore-owner']
+  local notfaction = fieldvalues['ignore-same-faction']
   local count = 0
   for di=i-range,i+range do
     for dj=j-range,j+range do
       if di ~= i or dj ~= j then
         local body = sector:getBodyAt(di, dj)
         if body and body:isSpec(specname)
-                and _checkOwner(actor, body, notowner) then
+                and _checkOwner(actor, body, notowner)
+                and _checkFaction(actor, body, notfaction) then
           count = count + 1
         end
       end
--- game/domain/operators/find_nearest_body.lua
+++ game/domain/operators/find_nearest_body.lua
@@ -6,6 +6,8 @@ OP.schema = {
   { id = 'range', name = "Range", type = 'value', match = 'integer',
     range = {0} },
   { id = 'ignore-owner', name = "Ignore Owner", type = 'boolean' },
+  { id = 'ignore-same-faction', name = "Ignore Same Faction",
+    type = 'boolean' },
   { id = 'output', name = "Label", type = 'output' }
 }
 
@@ -15,11 +17,16 @@ local function _checkOwner(actor, body, ignore)
   return not ignore or actor:getBody() ~= body
 end
 
+local function _checkFaction(actor, body, ignore)
+  return not ignore or actor:getBody():getFaction() ~= body:getFaction()
+end
+
 function OP.process(actor, fieldvalues)
   local sector = actor:getBody():getSector()
   local i, j = unpack(fieldvalues['pos'])
   local range = fieldvalues['range']
   local notowner = fieldvalues['ignore-owner']
+  local notfaction = fieldvalues['ignore-same-faction']
   local nearest
   local mindist = range+1
   for di=i-range,i+range do
@@ -27,7 +34,8 @@ function OP.process(actor, fieldvalues)
       if di ~= i or dj ~= j then
         local body = sector:getBodyAt(di, dj)
         local dist = math.max(math.abs(di-i), math.abs(dj-j))
-        if body and dist < mindist and _checkOwner(actor, body, notowner) then
+        if body and dist < mindist and _checkOwner(actor, body, notowner)
+                and _checkFaction(actor, body, notfaction) then
           nearest = body
           mindist = dist
         end
