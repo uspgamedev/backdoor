50dbdf6ef0a9cf0411acd6d71ca6a393dcd73a05
orenjiakira <orenjiakira@gmail.com>
2018-05-03 18:59:26 -0300

    Move themes to zones

--- game/database/schema/theme.lua
+++ game/database/schema/theme.lua
@@ -1,5 +1,6 @@
 
 return {
   { id = "bgm", name = "BGM", type = 'enum', options = 'resources.bgm' },
+  { id = "tileset", name = "TileSet", type = 'enum', options = 'resources.tileset' },
 }
 
--- game/database/schema/zone.lua
+++ game/database/schema/zone.lua
@@ -1,8 +1,8 @@
 
 return {
   { id = 'name', name = "Zone Name", type = 'string' },
-  { id = 'tileset', name = "Tile Set", type = 'enum',
-    options = "resources.tileset" },
+  { id = 'theme', name = "Zone Theme", type = 'enum',
+    options = "domains.theme"},
   { id = 'difficulty', name = "Difficulty Level", type = 'integer',
     range = {0,100} },
 }
