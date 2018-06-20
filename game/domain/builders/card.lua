e803aea27d9717ad94e4509cb5383ae6c06417f6
orenjiakira <orenjiakira@gmail.com>
2018-06-20 19:36:52 -0300

    Update builders to add ids to cards

--- game/domain/builders/card.lua
+++ game/domain/builders/card.lua
@@ -3,15 +3,16 @@ local Card = require 'domain.card'
 
 local BUILDER = {}
 
-function BUILDER.buildState(specname)
+function BUILDER.buildState(idgenerator, specname)
   return {
+    id = idgenerator.newID(),
     specname = specname,
     usages = 0,
   }
 end
 
-function BUILDER.buildElement(specname)
-  local state = BUILDER.buildState(specname)
+function BUILDER.buildElement(idgenerator, specname)
+  local state = BUILDER.buildState(idgenerator, specname)
   local card = Card(specname)
   card:loadState(state)
   return card
