631a98036aae3a5b207ec4a45d175f7a031c0ca5
orenjiakira <orenjiakira@gmail.com>
2018-06-27 17:13:19 -0300

    Remove actor hand limit attribute

--- game/domain/effects/new_hand.lua
+++ game/domain/effects/new_hand.lua
@@ -1,15 +1,17 @@
 
+local DEFS = require 'domain.definitions'
+
 local FX = {}
 
 FX.schema = {
-  {id='nothing', type='none', name = "NO PARAM"}
+  { id='nothing', type='none', name = "NO PARAM" }
 }
 
 function FX.process (actor, fieldvalues)
   if actor:isHandEmpty() then
-    for i = 1,actor:getHandLimit() do
+    for i = 1, DEFS.HAND_LIMIT do
       actor:drawCard()
-    end 
+    end
   end
 end
 
