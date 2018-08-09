af7446d6ede803d32c019f7c8d388909bd0b85cd
orenjiakira <orenjiakira@gmail.com>
2018-08-09 18:38:28 -0300

    Set user preferences influence and schematics

--- game/helpers/playsfx.lua
+++ game/helpers/playsfx.lua
@@ -1,9 +1,10 @@
 
+local PROFILE = require 'infra.profile'
 local RES = require 'resources'
 
 return function (sfxname)
   local sfx = RES.loadSFX(sfxname)
-  sfx:setVolume(1)
+  sfx:setVolume(PROFILE.getPreference("sfx-volume"))
   sfx:stop()
   return sfx:play()
 end
