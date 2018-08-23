e3362b470e3dbc35f8625e253a746704e6632b8c
orenjiakira <orenjiakira@gmail.com>
2018-08-23 18:57:25 -0300

    Use love's new setget CRequirePath methods

--- game/libs/init.lua
+++ game/libs/init.lua
@@ -1,5 +1,5 @@
 
 local fs = love.filesystem
 fs.setRequirePath("libs/?/init.lua;libs/?.lua;"..fs.getRequirePath())
-package.cpath = fs.getSourceBaseDirectory().."/?.so;" .. package.cpath
+fs.setCRequirePath(fs.getSourceBaseDirectory().."/?.so;"..fs.getCRequirePath())
 
