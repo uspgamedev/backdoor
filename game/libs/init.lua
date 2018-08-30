2216d12df5df165fd3903e39af5bdb6f296f2f1f
orenjiakira <orenjiakira@gmail.com>
2018-08-30 16:38:45 -0300

    Add dll path as well

--- game/libs/init.lua
+++ game/libs/init.lua
@@ -2,4 +2,5 @@
 local fs = love.filesystem
 fs.setRequirePath("libs/?/init.lua;libs/?.lua;"..fs.getRequirePath())
 fs.setCRequirePath(fs.getSourceBaseDirectory().."/?.so;"..fs.getCRequirePath())
+fs.setCRequirePath(fs.getSourceBaseDirectory().."/?.dll;"..fs.getCRequirePath())
 
