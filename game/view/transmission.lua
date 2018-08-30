342f25c358533233aeaed8dc0cc3e83dbcc0acc0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-08-30 16:40:37 -0300

    Width-based laser animation

--- game/view/transmission.lua
+++ game/view/transmission.lua
@@ -8,44 +8,29 @@ local Transmission = Class{
 
 local _SPD = 40
 local _COOLDOWN = 0.1
+local _WIDTH = 8
 
 function Transmission:init(origin, target)
   ELEMENT.init(self)
   self.target = target
   self.start = origin:clone()
-  self.finish = origin:clone()
-  self.cooldown = 0
-  self.state = 'expanding'
+  self.finish = self.target:getPoint()
+  self.width_scale = 1
   self.warmup = 0.05
+  self:addTimer("start", MAIN_TIMER, "tween", 0.5, self,
+                { width_scale = 0 }, 'in-back',
+                function () self:kill() end)
 end
 
 function Transmission:update(dt)
   self.warmup = math.max(self.warmup - dt, 0)
-  local targetpos = self.target:getPoint()
-  if self.state == 'expanding' then
-    self.finish = self.finish + (targetpos - self.finish) * dt * _SPD
-    if (self.finish - targetpos):len() < 2 then
-      self.finish = targetpos
-      self.state = 'cooldown'
-    end
-  elseif self.state == 'cooldown' then
-    self.cooldown = self.cooldown + dt
-    if self.cooldown >= _COOLDOWN then
-      self.state = 'contracting'
-    end
-  elseif self.state == 'contracting' then
-    self.start = self.start + (targetpos - self.start) * dt * _SPD
-    if (self.start - targetpos):len() < 2 then
-      self.start = targetpos
-      self:kill()
-    end
-  end
+  self.finish = self.target:getPoint()
 end
 
 function Transmission:draw()
   if self.warmup > 0 then return end
   local g = love.graphics
-  g.setLineWidth(4)
+  g.setLineWidth(_WIDTH * self.width_scale)
   g.setColor(COLORS.NEUTRAL)
   g.line(self.start.x, self.start.y, self.finish.x, self.finish.y)
 end
