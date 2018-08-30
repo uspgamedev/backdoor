52c791b99e65f5db5aa338db48b2c437182ac977
orenjiakira <orenjiakira@gmail.com>
2018-08-30 18:18:55 -0300

    Fix some other globals

--- game/view/consumelist.lua
+++ game/view/consumelist.lua
@@ -126,7 +126,7 @@ function View:selectPrev(n)
   self.holdbar:reset()
 end
 
-function View:selectNext()
+function View:selectNext(n)
   if self:isLocked() then return end
   n = n or 1
   self.selection = _next_circular(self.selection, #self.card_list, n)
