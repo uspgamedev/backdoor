c1a887b66390056bbe1d4d0eebfa4adfd99d526b
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-11-22 16:59:27 -0200

    Widget slots are visible again
    But they are just dummies

--- game/view/actorpanel/widgets/init.lua
+++ game/view/actorpanel/widgets/init.lua
@@ -1,10 +1,49 @@
 
-local Node = require 'view.node'
+local PLACEMENTS  = require 'domain.definitions.placements'
+local FONT        = require 'view.helpers.font'
+local COLORS      = require 'domain.definitions.colors'
+local Node        = require 'view.node'
+local Slot        = require 'view.actorpanel.widgets.slot'
+
+local _PD = 4
+local _MG = 24
+
+local _WIDGETSTRING = {
+  "PLACEMENTS",
+  "TRAITS",
+  "CONDITIONS",
+}
 
 local Widgets = Class({ __includes = { Node } })
 
-function Widgets:init(actor)
+function Widgets:init(actor, x, y)
   Node.init(self)
+  self:setPosition(x, y)
+  self.font = FONT.get("Text", 20)
+  self.slots = {}
+  local sqsize = Slot.SQRSIZE
+  for i=1,3 do
+    self.slots[i] = {}
+    for j=1,5 do
+      local slot = Slot((j - 1) * (sqsize + _PD),
+                        i*(_MG*2 + self.font:getHeight()),
+                        i == 1 and PLACEMENTS[PLACEMENTS[j]])
+      self.slots[i][j] = slot
+      self:addChild(slot)
+    end
+  end
+end
+
+function Widgets:render(g)
+  self.font:set()
+  g.push()
+  for i=1,3 do
+    g.translate(0, _MG*2)
+    g.setColor(COLORS.NEUTRAL)
+    g.print(_WIDGETSTRING[i], 0, 0)
+    g.translate(0, self.font:getHeight())
+  end
+  g.pop()
 end
 
 return Widgets
--- game/view/actorpanel/widgets/slot.lua
+++ game/view/actorpanel/widgets/slot.lua
@@ -1,30 +1,50 @@
 
-local Node = require 'view.node'
+local vec2    = require 'cpml' .vec2
+local Node    = require 'view.node'
+local FONT    = require 'view.helpers.font'
+local COLORS  = require 'domain.definitions.colors'
+
+local _MG = 24
+local _PD = 4
 
 local Slot = Class({ __includes = { Node } })
 
-function Slot:init(widget)
+Slot.SQRSIZE = 36
+
+function Slot:init(x, y, label)
   Node.init(self)
-  self:setWidget(widget)
+  self:setPosition(x, y)
+  self.label = label and label:lower()
 end
 
 function Slot:setWidget(widget)
   self.widget = widget or false
 end
 
+function Slot:swap(other)
+  self.widget, other.widget = other.widget, self.widget
+end
+
+function Slot:getPoint()
+  return vec2(self.position:unpack())
+end
+
 function Slot:render(g)
+  local sqsize = Slot.SQRSIZE
   local widget = self.widget
+  g.setColor(COLORS.EMPTY)
+  g.rectangle("fill", 0, 0, sqsize, sqsize)
   if widget then
     local icon = RES.loadTexture(widget:getIconTexture() or 'icon-none')
     local iw, ih = icon:getDimensions()
     icon:setFilter('linear', 'linear')
     g.setColor(COLORS[widget:getRelatedAttr()])
-    g.rectangle("fill", 0, 0, _SQRSIZE, _SQRSIZE)
+    g.rectangle("fill", 0, 0, sqsize, sqsize)
     g.setColor(COLORS.BLACK)
-    g.draw(icon, 0, 0, 0, _SQRSIZE/iw, _SQRSIZE/ih)
-  elseif wtype == 1 then
+    g.draw(icon, 0, 0, 0, sqsize/iw, sqsize/ih)
+  elseif self.label then
     g.setColor(COLORS.BLACK)
-    g.printf(PLACEMENTS[PLACEMENTS[i]]:lower(), 0, 0, _SQRSIZE, "center")
+    g.printf(self.label, 0, 0, sqsize, "center")
   end
 end
 
