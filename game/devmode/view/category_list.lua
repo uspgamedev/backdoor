47a08ecc435dcba1bc52a01b9eb99d11b23d0acf
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-11-29 18:19:55 -0200

    Add search field for DB categories

--- game/devmode/view/category_list.lua
+++ game/devmode/view/category_list.lua
@@ -3,6 +3,7 @@ local IMGUI = require 'imgui'
 local DB = require 'database'
 local DEFS = require 'domain.definitions'
 
+
 local function add(list, specname)
   table.insert(list, specname)
   list.n = list.n + 1
@@ -12,14 +13,32 @@ local function sort(list)
   table.sort(list)
 end
 
+local function filter(list, filtered, search_str)
+  if search_str == "" then
+    return list
+  else
+    filtered.n = 0
+    for i=1,list.n do
+      if list[i]:match(search_str) then
+        filtered.n = filtered.n + 1
+        filtered[filtered.n] = list[i]
+      end
+    end
+    return filtered
+  end
+end
+
 return function(category_name, group_name, title)
   local group = DB.loadGroup(category_name, group_name)
-  local list = { n = 0 }
+  local fulllist = { n = 0 }
+  local filtered = { n = 0 }
+  local search_str = ""
+  local list = fulllist
   local selected = 0
   for name in DB.listItemsIn(category_name, group_name) do
-    add(list, name)
+    add(fulllist, name)
   end
-  sort(list)
+  sort(fulllist)
 
   local function delete()
     local specname = list[selected]
@@ -35,8 +54,8 @@ return function(category_name, group_name, title)
         new[key.id] = new[key.id] or {}
       end
     end
-    add(list, specname)
-    sort(list)
+    add(fulllist, specname)
+    sort(fulllist)
     for i,name in DB.listItemsIn(category_name, group_name) do
       if name == specname then
         selected = i
@@ -58,8 +77,14 @@ return function(category_name, group_name, title)
     if IMGUI.Button("New "..title) then
       gui:push('name_input', title, newvalue)
     end
-    IMGUI.Text(("All %ss:"):format(title))
     local changed
+    IMGUI.PushItemWidth(120)
+    search_str, changed = IMGUI.InputText("Search", search_str, 32)
+    IMGUI.PopItemWidth()
+    IMGUI.Text(("All %ss:"):format(title))
+    if changed then
+      list = filter(fulllist, filtered, search_str)
+    end
     selected, changed = IMGUI.ListBox("", selected, list, list.n, 15)
     if changed and selected then
       gui:push('specification_editor', group[list[selected]], group_name,
