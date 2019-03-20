025f6073e782a227970d0af38fae8e0cf052dafc
rilifon <pudimdefrutas@gmail.com>
2019-03-20 18:40:28 -0300

    Effect on dialogue boxes

--- game/view/dialoguebox.lua
+++ game/view/dialoguebox.lua
@@ -7,7 +7,10 @@ local FONT     = require 'view.helpers.font'
 
 local _TILE_W = VIEWDEFS.TILE_W
 local _TILE_H = VIEWDEFS.TILE_H
+local _FX_MAGNITUDE = 6
+local _FX_SPEED = 2.5
 local _font = FONT.get('Text', 20)
+
 
 -- Class
 
@@ -34,17 +37,24 @@ end
 function DialogueBox:draw()
   local g = love.graphics
   local x, y = self:getPosition()
+
+  g.push()
+  g.translate(x, y)
+
   --Draw bg
   g.setColor(COLORS.HUD_BG)
-  g.rectangle("fill", x, y, self.w, self.h)
+  g.rectangle("fill", 0, 0, self.w, self.h)
   g.setColor(COLORS.NEUTRAL)
   g.setLineWidth(3)
-  g.rectangle("line", x, y, self.w, self.h)
+  g.rectangle("line", 0, 0, self.w, self.h)
 
   --Draw text
   g.setColor(COLORS.NEUTRAL)
   _font:set()
-  g.print(self.text, x + 10, y)
+  _font:setFilter("linear", "linear")
+  g.print(self.text, 0 + 10, 0)
+
+  g.pop()
 end
 
 function DialogueBox:setSide(side)
@@ -62,9 +72,10 @@ function DialogueBox:getPosition()
     error("not a valid side for dialogue box")
   end
 
-  y = (self.i+.5)*_TILE_H - self.h/2 + self.y_offset
+  local fx_offset = math.sin(love.timer.getTime() * _FX_SPEED) * _FX_MAGNITUDE
+  y = (self.i+.5)*_TILE_H - self.h/2 + self.y_offset + fx_offset
 
-  return x, y
+  return math.floor(x + .5), math.floor(y + .5)
 end
 
 function DialogueBox:getSize()
