55f2fa33c808aaea35a2099e145cf57e161e2661
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-04-17 19:14:45 -0300

    Turn ability editor into a closure-class

--- game/devmode/view/input/ability.lua
+++ game/devmode/view/input/ability.lua
@@ -2,6 +2,11 @@
 local IMGUI       = require 'imgui'
 local DB          = require 'database'
 local IDGenerator = require 'common.idgenerator'
+local class       = require 'lux.class'
+
+local table = table
+local ipairs = ipairs
+local setfenv = setfenv
 
 local _CMDTYPES = {
   'inputs', 'effects',
@@ -10,105 +15,112 @@ local _CMDTYPES = {
 }
 
 local _idgen = IDGenerator()
-local inputs = {}
 
-local function _commandList(gui, ability, cmdtype, selected, delete)
+local AbilityEditor = class:new()
 
-  local result
-  local cmdoptions = {}
-  local cmdtypes = {}
+function AbilityEditor:instance(obj, _elementspec, _fieldschema)
 
-  local list = ability[cmdtype] or {}
-  ability[cmdtype] = list
+  setfenv(1, obj)
 
-  for _,option in DB.subschemaTypes(cmdtype) do
-    table.insert(cmdoptions, option)
-    table.insert(cmdtypes, cmdtype:sub(1,-2))
+  local _ability = _elementspec[_fieldschema.id] or
+                   { inputs = {}, effects = {} }
+  local _selected = nil
+  local _active = not (not _elementspec[_fieldschema.id] and
+                           _fieldschema.optional)
+
+  local function _delete()
+    table.remove(_ability[_selected.cmdtype], _selected.idx)
   end
 
-  for _,option in DB.subschemaTypes('operators') do
-    table.insert(cmdoptions, option)
-    table.insert(cmdtypes, 'operator')
-  end
+  local function _commandList(gui, cmdtype)
 
-  IMGUI.Text(("%ss:"):format(_CMDTYPES[cmdtype]))
-  IMGUI.Indent(20)
-  for i,command in ipairs(list) do
-    local view
-    if command.output then
-      view = ("%s -> [%s]"):format(command.name, command.output)
-    else
-      view = ("%2d: %s"):format(i, command.name)
+    local result
+    local cmdoptions = {}
+    local cmdtypes = {}
+
+    local list = _ability[cmdtype] or {}
+    _ability[cmdtype] = list
+
+    for _,option in DB.subschemaTypes(cmdtype) do
+      table.insert(cmdoptions, option)
+      table.insert(cmdtypes, cmdtype:sub(1,-2))
     end
-    IMGUI.PushID(("%s/%s:%d"):format(ability, cmdtype, i))
-    if IMGUI.Selectable(view,
-                        selected and selected.cmdtype == cmdtype
-                                 and selected.idx == i) then
-      selected = selected or {}
-      selected.cmdtype = cmdtype
-      selected.idx = i
-      gui:push('specification_editor', command,
-               command.type .. 's/' .. command.name, _CMDTYPES[cmdtype], delete,
-               nil, ability)
+
+    for _,option in DB.subschemaTypes('operators') do
+      table.insert(cmdoptions, option)
+      table.insert(cmdtypes, 'operator')
     end
-    IMGUI.PopID()
-  end
-  if IMGUI.Button("New " .. _CMDTYPES[cmdtype]) then
-    gui:push(
-      'list_picker', _CMDTYPES[cmdtype], cmdoptions,
-      function (value)
-        if value then
-          local new = { type = cmdtypes[value], name = cmdoptions[value] }
-          local schema = require('domain.'..new.type..'s.'..new.name).schema
-          for _,subfield in ipairs(schema) do
-            if subfield.type == 'output' then
-              new[subfield.id] = 'label'.._idgen.newID()
-            end
-          end
-          table.insert(list, new)
-          return true
-        end
-        return 0
+
+    IMGUI.Text(("%ss:"):format(_CMDTYPES[cmdtype]))
+    IMGUI.Indent(20)
+    for i,command in ipairs(list) do
+      local view
+      if command.output then
+        view = ("%s -> [%s]"):format(command.name, command.output)
+      else
+        view = ("%2d: %s"):format(i, command.name)
       end
-    )
+      IMGUI.PushID(("%s/%s:%d"):format(_ability, cmdtype, i))
+      if IMGUI.Selectable(view,
+                          _selected and _selected.cmdtype == cmdtype
+                                    and _selected.idx == i) then
+        _selected = _selected or {}
+        _selected.cmdtype = cmdtype
+        _selected.idx = i
+        gui:push('specification_editor', command,
+                 command.type .. 's/' .. command.name, _CMDTYPES[cmdtype],
+                 _delete, nil, _ability)
+      end
+      IMGUI.PopID()
+    end
+    if IMGUI.Button("New " .. _CMDTYPES[cmdtype]) then
+      gui:push(
+        'list_picker', _CMDTYPES[cmdtype], cmdoptions,
+        function (value)
+          if value then
+            local new = { type = cmdtypes[value], name = cmdoptions[value] }
+            local schema = require('domain.'..new.type..'s.'..new.name).schema
+            for _,subfield_schema in ipairs(schema) do
+              if subfield_schema.type == 'output' then
+                new[subfield_schema.id] = 'label'.._idgen.newID()
+              end
+            end
+            table.insert(list, new)
+            return true
+          end
+          return 0
+        end
+      )
+    end
+    IMGUI.Unindent(20)
   end
-  IMGUI.Unindent(20)
-  return selected
-end
 
-function inputs.ability(spec, field)
-
-  local ability = spec[field.id]
-                  or { inputs = {}, effects = {} }
-  local selected = nil
-
-  local function delete()
-    table.remove(ability[selected.cmdtype], selected.idx)
-  end
-
-  local _active = not (not spec[field.id] and field.optional)
-
-  return function(gui)
-    if field.optional then
-      IMGUI.PushID(field.id .. ".check")
+  function input(gui)
+    if _fieldschema.optional then
+      IMGUI.PushID(_fieldschema.id .. ".check")
       _active = IMGUI.Checkbox("", _active)
       IMGUI.PopID()
       IMGUI.SameLine()
     end
-    IMGUI.Text(("%s"):format(field.name))
+    IMGUI.Text(("%s"):format(_fieldschema.name))
     if _active then
       IMGUI.Indent(20)
-      if field.hint then
-        IMGUI.Text(field.hint)
+      if _fieldschema.hint then
+        IMGUI.Text(_fieldschema.hint)
       end
       for _,cmdtype in ipairs(_CMDTYPES) do
-        selected = _commandList(gui, ability, cmdtype, selected, delete)
+        _commandList(gui, cmdtype)
       end
-      spec[field.id] = ability
+      _elementspec[_fieldschema.id] = _ability
       IMGUI.Unindent(20)
     end
   end
+
+  function __operator:call(gui)
+    return obj.input(gui)
+  end
+
 end
 
-return inputs
+return { ability = AbilityEditor }
 
