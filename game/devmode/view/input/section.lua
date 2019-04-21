b6fce112b86cccca5a8fd9330035ce1313d15605
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-04-20 21:36:16 -0300

    Turn section input editor into closuse-class

--- game/devmode/view/input/section.lua
+++ game/devmode/view/input/section.lua
@@ -2,52 +2,66 @@
 local IMGUI = require 'imgui'
 local INPUT = require 'devmode.view.input'
 local DB    = require 'database'
+local class = require 'lux.class'
 
-local _inputs = {}
+local setfenv = setfenv
+local table = table
+local ipairs = ipairs
+local type = type
+local require = require
 
-function _inputs.section(spec, field, parent)
+local SectionEditor = class:new()
 
-  local schema = field.schema
-  local backup = {}
-  local inside_schema = {}
+function SectionEditor:instance(obj, _elementspec, _fieldschema, _parent)
 
-  if type(schema) == "string" then
-    schema = require ('domain.'..schema).schema
+  setfenv(1, obj)
+
+  local _subschema = _fieldschema.schema
+  local _backup = {}
+  local _subfield_inputs = {}
+
+  if type(_subschema) == "string" then
+    _subschema = require ('domain.'.._subschema).schema
   end
 
-  for i, subfield in ipairs(schema) do
+  for i, subfield_schema in ipairs(_subschema) do
     local input_lambda
-    inside_schema[i] = {
-      id = ("%s:%s"):format(field.id, subfield.id),
+    _subfield_inputs[i] = {
+      id = ("%s:%s"):format(_fieldschema.id, subfield_schema.id),
       input = function(section_spec)
-        input_lambda = input_lambda or INPUT(section_spec, subfield, parent)
+        input_lambda = input_lambda or INPUT(section_spec, subfield_schema,
+                                             _parent)
         return input_lambda
       end
     }
   end
 
-  return function(gui)
-    local element = spec[field.id]
+  function input(gui)
+    local element = _elementspec[_fieldschema.id]
     local enabled
-    enabled = IMGUI.Checkbox(field.name, not not element)
-    if not element and (enabled or field.required) then
-      element = backup
-    elseif element and not enabled and not field.required then
-      backup = element
+    enabled = IMGUI.Checkbox(_fieldschema.name, not not element)
+    if not element and (enabled or _fieldschema.required) then
+      element = _backup
+    elseif element and not enabled and not _fieldschema.required then
+      _backup = element
       element = false
     end
     if element then
       IMGUI.Indent(20)
-      for i, subfield in ipairs(schema) do
-        IMGUI.PushID(inside_schema[i].id)
-        inside_schema[i].input(element)(gui)
+      for i, subfield_schema in ipairs(_subschema) do
+        IMGUI.PushID(_subfield_inputs[i].id)
+        _subfield_inputs[i].input(element)(gui)
         IMGUI.PopID()
       end
       IMGUI.Unindent(20)
     end
-    spec[field.id] = element
+    _elementspec[_fieldschema.id] = element
+  end
+
+  function __operator:call(gui)
+    return obj.input(gui)
   end
 end
 
-return _inputs
+return { section = SectionEditor }
 
