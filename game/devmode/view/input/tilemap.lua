7af58a3f9dbf87911fc48914abebeda87d5eea06
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-04-25 12:24:44 -0300

    Change input type to button matrix

--- game/devmode/view/input/tilemap.lua
+++ game/devmode/view/input/tilemap.lua
@@ -6,9 +6,18 @@ local setfenv = setfenv
 local print = print
 local max, min = math.max, math.min
 
+--- Input editor for tilemap fields.
+--  Schema:
+--  { id = 'internal-identifier', name = "Visible Label",
+--    minwidth = 1, maxwidth = <minwidth>,
+--    minheight = 1, maxheight = <minheight>,
+--    palette = { ' ', '.' } }
+--  Spec:
+--  { width = <minwidth>, height = <minheight>, data = { '.', ... } }
 local TileMapEditor = class:new()
 
-function TileMapEditor.instance(_, obj, _elementspec, _fieldschema)
+-- luacheck: no self
+function TileMapEditor:instance(obj, _elementspec, _fieldschema)
 
   setfenv(1, obj)
 
@@ -16,11 +25,12 @@ function TileMapEditor.instance(_, obj, _elementspec, _fieldschema)
                                 _fieldschema.minheight or 1
   local _maxwidth, _maxheight = _fieldschema.maxwidth or _minwidth,
                                 _fieldschema.maxheight or _minheight
+  local _palette = _fieldschema.palette
 
   local _tilemap = _elementspec[_fieldschema.id] or {
     width = _minwidth,
     height = _minheight,
-    data = ""
+    data = {}
   }
 
   local function _clampDimensions()
@@ -32,65 +42,49 @@ function TileMapEditor.instance(_, obj, _elementspec, _fieldschema)
     _tilemap.width = newwidth
     _tilemap.height = newheight
     _clampDimensions()
-    local newdata = ""
+    local newdata = {}
     for i = 1, newheight do
       for j = 1, newwidth do
-        local tile = ''
+        local tile = 1
         if i <= _tilemap.height or j <= _tilemap.width then
           local idx = (i-1) * _tilemap.width + j
-          tile = _tilemap.data:sub(idx, idx)
+          tile = _tilemap.data[idx] or tile
         end
-        if tile == '' then tile = '.' end
-        newdata = newdata .. tile
+        newdata[(i-1) * newwidth + j] = tile
       end
     end
     _tilemap.data = newdata
-  end
-
-  local function _toText()
-    local text = ""
-    for i = 1, _tilemap.height do
-      local begin = 1 + (i-1)*_tilemap.width
-      text = text .. _tilemap.data:sub(begin, begin + _tilemap.width-1) .. '\n'
-    end
-    print(text)
-    return text
-  end
-
-  local function _fromText(text)
-    print("apply")
-    local newdata = ""
-    local scan, last = 1, 1
-    print(text)
-    while scan <= #text do
-      local newchar = text:sub(scan, scan)
-      local oldchar = _tilemap.data:sub(last, last)
-      if newchar ~= '\n' then
-        newdata = newdata .. newchar
-        last = last + 1
-        if newchar ~= oldchar then
-          scan = scan + 1
-        end
-      end
-      scan = scan + 1
-    end
-    _tilemap.data = newdata
-    print(_toText())
   end
 
   _resize(_tilemap.width, _tilemap.height)
 
   _elementspec[_fieldschema.id] = _tilemap
 
-  function input(gui)
+  function input(_)
     IMGUI.PushID(_fieldschema.id)
     IMGUI.Text(_fieldschema.name)
-    local newwidth, newheight, changed =
-      IMGUI.InputInt2('Dimensions', _tilemap.width, _tilemap.height)
-    if changed then _resize(newwidth, newheight) end
-    local length = (_tilemap.width+1) * _tilemap.height + 2
-    local text, changed = IMGUI.InputTextMultiline("Tilemap", _toText(), length)
-    if changed then _fromText(text) end
+    do -- dimensions editor
+      local newwidth, newheight, changed =
+        IMGUI.InputInt2('Dimensions', _tilemap.width, _tilemap.height)
+      if changed then _resize(newwidth, newheight) end
+    end
+    do -- tiles editor
+      for i = 1, _tilemap.height do
+        for j = 1, _tilemap.width do
+          local idx = (i-1) * _tilemap.width + j
+          local tile = _tilemap.data[idx]
+          local glyph = _palette[tile]
+          if IMGUI.SmallButton(glyph) then
+            tile = (tile % #_palette) + 1
+            _tilemap.data[idx] = tile
+            print(tile)
+          end
+          if j < _tilemap.width then
+            IMGUI.SameLine()
+          end
+        end
+      end
+    end
     IMGUI.PopID()
   end
 
