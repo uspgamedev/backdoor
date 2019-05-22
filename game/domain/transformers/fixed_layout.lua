55a2277ae46f4ed429b24467c1a28d48c7c708ee
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-05-22 19:09:15 -0300

    Allow placing exits

--- game/domain/transformers/fixed_layout.lua
+++ game/domain/transformers/fixed_layout.lua
@@ -21,7 +21,7 @@ TRANSFORMER.schema = {
   { id = 'offset', name = "Tilemap offset", type = 'vector', size = 2,
     range = { 0, 50 } },
   { id = 'player-pos', name = "Player position", type = 'vector', size = 2,
-    range = { 0, 50 }, optional = true },
+    range = { 0, 50 } },
 }
 
 function TRANSFORMER.process(sectorinfo, params)
@@ -30,12 +30,21 @@ function TRANSFORMER.process(sectorinfo, params)
   local player_x, player_y = unpack(params['player-pos'])
   local mw, mh = sectorinfo.grid.getMargins()
 
+  local last_exit_id = nil
+
   for y = 0, map.height - 1 do
     for x = 0, map.width - 1 do
       local real_x, real_y = 1 + mw + ox + x, 1 + mh + oy + y
       local raw = map.data[1 + y * map.width + x]
       local fill = PALETTE[raw]
       sectorinfo.grid.set(real_x, real_y, fill)
+      if fill == SCHEMATICS.EXIT then
+        local exit
+        last_exit_id, exit = next(sectorinfo.exits, last_exit_id)
+        if last_exit_id then
+          exit.pos = { real_y, real_x }
+        end
+      end
     end
   end
 
