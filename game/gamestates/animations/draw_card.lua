e3a42db7a4f6f11fa8144d650ffbf5f2a20e8211
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-09-16 16:19:28 -0300

    Discard animation

--- game/gamestates/animations/draw_card.lua
+++ game/gamestates/animations/draw_card.lua
@@ -3,7 +3,6 @@
 
 local TweenValue    = require 'view.helpers.tweenvalue'
 local CardView      = require 'view.card'
-local vec2          = require 'cpml' .vec2
 
 local ANIM = require 'common.activity' ()
 
@@ -13,15 +12,11 @@ function ANIM:script(route, view, report)
   if report.actor == route:getControlledActor() then
     local cardview = CardView(report.card)
     local frontbuffer = view.frontbuffer
-    local hand = view.action_hud.handview
-    local start = vec2(frontbuffer:getPosition())
-    local finish = vec2(hand:positionForIndex(hand:cardCount()))
+    local hand = action_hud.handview
     hand:addCard(cardview)
-    action_hud:disableCardInfo()
-    cardview:setOffset((start - finish):unpack())
     cardview:register("HUD")
-    cardview:addTimer("slide", MAIN_TIMER, "tween", 0.5, cardview,
-                      { offset = vec2() }, 'out-cubic')
+    cardview:setPosition(frontbuffer:getPosition())
+    action_hud:disableCardInfo()
     self.wait(delay:set(0.2))
     cardview:setDrawTable("HUD_FX")
   end
