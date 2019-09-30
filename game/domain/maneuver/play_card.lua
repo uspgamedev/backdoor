aa746772975df08547182a6364665422c7b072af
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-09-30 15:16:49 -0300

    Temporary fix for widgets

--- game/domain/maneuver/play_card.lua
+++ game/domain/maneuver/play_card.lua
@@ -53,13 +53,13 @@ function PLAYCARD.perform(actor, inputvalues)
     ABILITY.execute(card:getArtAbility(), actor, inputvalues)
     body:triggerWidgets(TRIGGERS.ON_ACT)
   elseif card:isWidget() then
+    body:placeWidget(card)
     coroutine.yield('report', {
       type = 'play_widget_card',
       actor = actor,
       card_index = inputvalues.card_index
     })
     actor:playCard(inputvalues.card_index)
-    body:placeWidget(card)
   end
 
   actor:checkFocus()
