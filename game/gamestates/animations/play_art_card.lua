6dbb689c6bc928eeb8ee965b779e595ea886c2a5
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-09-30 12:00:39 -0300

    Force card info during art animation

--- game/gamestates/animations/play_art_card.lua
+++ game/gamestates/animations/play_art_card.lua
@@ -23,11 +23,12 @@ function ANIM:script(route, view, report)
     _waitAndAnnounce(cardview, self.wait)
     action_hud.handview:keepFocusedCard(false)
     action_hud.handview:removeCard(report.card_index)
-    action_hud:disableCardInfo()
+    action_hud.handview.cardinfo:forceShow(cardview.card)
     cardview:setAlpha(1)
     cardview:setFocus(false)
     cardview:register("HUD_FX")
     _slideRight(cardview, backbuffer, self)
+    action_hud.handview.cardinfo:forceShow(false)
     if not cardview.temporary then
       _slideDown(cardview, backbuffer)
     else
@@ -55,7 +56,7 @@ function _slideRight(cardview, backbuffer, task)
                     { position = target_pos }, 'out-cubic',
                     function () task.resume() end)
   task.wait()
-  task.wait(delay:set(0.2))
+  task.wait(delay:set(0.6))
   delay:kill()
 end
 
