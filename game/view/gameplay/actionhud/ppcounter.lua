c6aed4912249ec9eb10d336173d14a7312ffcd70
rilifon <pudimdefrutas@gmail.com>
2019-10-21 11:03:54 -0300

    First version of ppcounter working

--- game/view/gameplay/actionhud/ppcounter.lua
+++ game/view/gameplay/actionhud/ppcounter.lua
@@ -1,8 +1,15 @@
 
 local Class      = require "steaming.extra_libs.hump.class"
 local vec2       = require 'cpml' .vec2
+local DEFS       = require 'domain.definitions'
 local COLORS     = require 'domain.definitions.colors'
 local ELEMENT    = require "steaming.classes.primitives.element"
+
+local _RADIUS = 28
+local _CIRCLE_WIDTH = 5
+local _SEPARATOR_WIDTH = 5
+
+local _stencil
 
 local PPCounter = Class{
   __includes = {ELEMENT}
@@ -11,15 +18,19 @@ local PPCounter = Class{
 function PPCounter:init()
   ELEMENT.init(self)
 
-  self.position = vec2()
-  self.pp = 0
+  self.pp = DEFS.MAX_PP
+  self.angle = 0
+
 end
 
 function PPCounter:draw()
   local g = love.graphics
 
   g.setColor(COLORS.PP)
-  g.circle("line", self.position.x, self.position.y, 30)
+  love.graphics.stencil(_stencil, "replace", 1)
+  love.graphics.setStencilTest("less", 1)
+  g.arc("fill", 0, 0, _RADIUS, -math.pi/2, -math.pi/2 + self.angle)
+  love.graphics.setStencilTest()
 end
 
 function PPCounter:setPP(value)
@@ -27,6 +38,22 @@ function PPCounter:setPP(value)
 end
 
 function PPCounter:update(dt)
+  local target_angle = (self.pp/DEFS.MAX_PP)*2*math.pi
+  self.angle = self.angle - (self.angle - target_angle)*.9*dt
+end
+
+--local functions
+function _stencil()
+  local g = love.graphics
+  g.push()
+  g.rotate(2*math.pi/DEFS.MAX_PP)
+  g.setLineWidth(_SEPARATOR_WIDTH)
+  for i = 1, DEFS.MAX_PP do
+    g.line(0, 0, 0, -_RADIUS-_CIRCLE_WIDTH)
+    g.rotate(2*math.pi/DEFS.MAX_PP)
+  end
+  g.pop()
+  g.circle("fill", 0, 0, _RADIUS - _CIRCLE_WIDTH)
 end
 
 return PPCounter
