cb4fdbcef42f0bb216a0df1103dede5117b4a8ad
rilifon <pudimdefrutas@gmail.com>
2019-10-21 15:06:17 -0300

    Using image for ppcounter

--- game/view/gameplay/actionhud/ppcounter.lua
+++ game/view/gameplay/actionhud/ppcounter.lua
@@ -4,15 +4,12 @@ local vec2       = require 'cpml' .vec2
 local DEFS       = require 'domain.definitions'
 local COLORS     = require 'domain.definitions.colors'
 local ELEMENT    = require "steaming.classes.primitives.element"
+local RES        = require 'resources'
 
 local _RADIUS = 35
 local _BAR_WIDTH = 10
 local _MARGIN = 8
 local _SPEED = 3*math.pi/2
-
-
-local _stencil
-local _draw_polygon
 
 local PPCounter = Class{
   __includes = {ELEMENT}
@@ -20,6 +17,8 @@ local PPCounter = Class{
 
 function PPCounter:init()
   ELEMENT.init(self)
+
+  self.texture = RES.loadTexture("wedge")
 
   self.pp = DEFS.MAX_PP
   self.angle = 0
@@ -30,16 +29,44 @@ function PPCounter:draw()
   local g = love.graphics
 
 
-  g.stencil(_stencil, "replace", 1)
-  g.setStencilTest("equal", 1)
+
   g.setColor(COLORS.HALF_VISIBLE)
-  g.circle("fill", 0, 0, _RADIUS)
+  self:draw_bars()
+
+  local stencil = function()
+                    local start_angle = math.pi/DEFS.MAX_PP
+                    love.graphics.arc("fill", 0, 0, _RADIUS, -math.pi/2 + start_angle, -math.pi/2 + self.angle + start_angle)
+                  end
+  g.stencil(stencil, "replace", 1)
   g.setStencilTest("equal", 1)
   g.setColor(COLORS.PP)
-  local start_angle = math.pi/DEFS.MAX_PP
-  g.arc("fill", 0, 0, _RADIUS, -math.pi/2 + start_angle, -math.pi/2 + self.angle + start_angle)
+  self:draw_bars()
   g.setStencilTest()
+
 end
+
+function PPCounter:draw_bars()
+  local g = love.graphics
+  local v1 = vec2(0, -_RADIUS)
+  local v2 = v1:rotate(math.pi/DEFS.MAX_PP)
+  v2 = v2 - v1
+  v2 = v2*((v2:len()-_MARGIN)/v2:len())
+  v2 = vec2(v1.x + v2.x, v1.y + v2.y)
+  local v3 = v2 - v1
+  v3.x = -v3.x
+  v3 = v1 + v3
+
+  local scale_x = (v2.x - v3.x)/self.texture:getWidth()
+  local scale_y = _BAR_WIDTH/self.texture:getHeight()
+
+  g.push()
+  for i = 1, DEFS.MAX_PP do
+    g.draw(self.texture, v3.x, v3.y, nil, scale_x, scale_y)
+    g.rotate(2*math.pi/DEFS.MAX_PP)
+  end
+  g.pop()
+end
+
 
 function PPCounter:setPP(value)
   self.pp = value
@@ -52,54 +79,6 @@ function PPCounter:update(dt)
   else
     self.angle = math.max(self.angle - _SPEED*dt, target_angle)
   end
-end
-
---local functions
-function _stencil()
-  local g = love.graphics
-  g.push()
-  for i = 1, DEFS.MAX_PP do
-    _draw_polygon()
-    g.rotate(2*math.pi/DEFS.MAX_PP)
-  end
-  g.pop()
-end
-
-function _draw_polygon()
-  local x, y = 0, -_RADIUS
-  local angle = 2*math.pi/DEFS.MAX_PP
-  local vertices = {} --Vertices of polygon
-  local add = function(value)
-                table.insert(vertices, value)
-              end
-  --add(x)
-  --add(y)
-  local v1 = vec2(x, y)
-  local v2 = v1:rotate(angle/2)
-  v2 = v2 - v1
-  v2 = v2*((v2:len()-_MARGIN)/v2:len())
-  v2 = vec2(x + v2.x, y + v2.y)
-  add(v2.x)
-  add(v2.y)
-  local v3 = v2*((v2:len() - _BAR_WIDTH)/v2:len())
-  add(v3.x)
-  add(v3.y)
-  local v4 = v1*((v1:len() - _BAR_WIDTH)/v1:len())
-  --add(v4.x)
-  --add(v4.y)
-  local v5 = v3 - v4
-  v5.x = -v5.x
-  v5 = v4 + v5
-  add(v5.x)
-  add(v5.y)
-  local v6 = v2 - v1
-  v6.x = -v6.x
-  v6 = v1 + v6
-  add(v6.x)
-  add(v6.y)
-
-  local g = love.graphics
-  g.polygon("fill", vertices)
 end
 
 return PPCounter
