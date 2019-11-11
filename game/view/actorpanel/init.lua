cdb8b4e521e9652f2fc559cd7e374dbedb0f4932
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-11-11 14:28:49 -0300

    Slide sidebar in/out with TAB

--- game/view/actorpanel/init.lua
+++ game/view/actorpanel/init.lua
@@ -1,4 +1,6 @@
 
+-- luacheck: globals MAIN_TIMER
+
 local COLORS = require 'domain.definitions.colors'
 
 local Node      = require 'view.node'
@@ -10,14 +12,16 @@ local LifeBar = require 'view.actorpanel.lifebar'
 local Stats   = require 'view.actorpanel.stats'
 local Widgets = require 'view.actorpanel.widgets'
 
-local ActorHudTree = Class({ __includes = { Node } })
+local Deferred = require 'common.deferred'
+
+local ActorHUDTree = Class({ __includes = { Node } })
 
 local _WIDTH, _HEIGHT = 320, 720
 local _MG = VIEWDEFS.PANEL_MG
 local _PD = 8
 local _INNERWIDTH = _WIDTH - 2*_PD - 2*_MG
 
-function ActorHudTree:init(actor)
+function ActorHUDTree:init(actor)
   Node.init(self)
   self.actor = actor
   -- subtrees for each section
@@ -30,15 +34,41 @@ function ActorHudTree:init(actor)
   self:addChild(self.lifebar)
   self:addChild(self.stats)
   self:addChild(self.widgets)
-  self:setPosition(960 + _MG, _MG)
+  self:setPosition(ActorHUDTree.HIDDEN_POSITION())
   return self:register("HUD_BG", nil, "actor_panel")
 end
 
-function ActorHudTree:getWidgets()
+function ActorHUDTree.HIDDEN_POSITION()
+  local w, _ = VIEWDEFS.VIEWPORT_DIMENSIONS()
+  return w + 2*_MG, _MG
+end
+
+function ActorHUDTree.VISIBLE_POSITION()
+  local w, _ = VIEWDEFS.VIEWPORT_DIMENSIONS()
+  return w - _WIDTH + _MG, _MG
+end
+
+function ActorHUDTree:getWidgets()
   return self.widgets
 end
 
-function ActorHudTree:render(g)
+function ActorHUDTree:hide()
+  return self:_slideTo(ActorHUDTree.HIDDEN_POSITION())
+end
+
+function ActorHUDTree:show()
+  return self:_slideTo(ActorHUDTree.VISIBLE_POSITION())
+end
+
+function ActorHUDTree:_slideTo(x, y)
+  local deferred = Deferred:new()
+  self:removeTimer("slide_sidebar", MAIN_TIMER)
+  self:addTimer("slide_sidebar", MAIN_TIMER, "tween", .5, self.position,
+                 { x = x, y = y }, 'out-cubic', function () deferred:andThen() end)
+  return deferred
+end
+
+function ActorHUDTree:render(g)
   g.setColor(1, 1, 1)
   g.setLineWidth(4)
   g.push()
@@ -50,4 +80,4 @@ function ActorHudTree:render(g)
   return self.nametext:draw(0, -8)
 end
 
-return ActorHudTree
+return ActorHUDTree
