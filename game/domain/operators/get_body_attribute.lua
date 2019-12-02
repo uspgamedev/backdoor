1bb9e8d52438476e8428e79c62e981154c47070c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-12-02 11:23:52 -0300

    Fix some card previews

--- game/domain/operators/get_body_attribute.lua
+++ game/domain/operators/get_body_attribute.lua
@@ -6,7 +6,6 @@ local DEFS = require 'domain.definitions'
 local OP = {}
 
 OP.schema = {
-  { id = 'body', name = "Body", type = 'value', match = 'body' },
   { id = 'which', name = "Attribute", type = 'enum',
     options = DEFS.BODY_ATTRIBUTES },
   { id = 'output', name = "Label", type = 'output' }
@@ -15,7 +14,7 @@ OP.schema = {
 OP.type = 'integer'
 
 function OP.process(actor, fieldvalues)
-  local body = fieldvalues['body']
+  local body = actor:getBody()
   return body["get"..fieldvalues.which](body)
 end
 
