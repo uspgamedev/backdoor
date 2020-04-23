2fb81078bc7338d046b191da4e6be45b0478c684
Rica <pudimdefrutas@gmail.com>
2020-04-23 15:30:19 -0300

    Added turn preview icons for all bodies

--- game/database/schema/appearance.lua
+++ game/database/schema/appearance.lua
@@ -1,5 +1,5 @@
 
 return {
   { id = "idle", name = "Idle", type = 'enum', options = 'resources.sprite' },
+  { id = "turn_icon", name = "Turn Icon", type = 'enum', options = 'resources.texture' },
 }
-
