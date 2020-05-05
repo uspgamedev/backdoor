a15a129d0a21cade3582033b19bd032463869e8d
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-05-05 17:22:49 -0300

    WIP tutorial map

--- game/domain/definitions/routemap.lua
+++ game/domain/definitions/routemap.lua
@@ -40,6 +40,7 @@ ROUTEMAP.initial_nodes = {
   --{ ZONES.K, SYMBOLS.e },
   --{ ZONES.L, SYMBOLS.e },
   --{ ZONES.M, SYMBOLS.e },
+  { ZONES.A, "tutorial" },
 }
 
 ROUTEMAP.initial_connections = {
@@ -53,6 +54,7 @@ ROUTEMAP.initial_connections = {
   {8, 9},
   {9, 10},
   {10, 11},
+  {12, 1},
 }
 
 --ROUTEMAP.initial_connections = {
