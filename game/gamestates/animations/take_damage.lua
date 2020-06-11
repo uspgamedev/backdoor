403ad6a3e116291d9e113d19b022cf02d6b0690c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-06-11 15:45:38 -0300

    Add frame-by-frame animation effect
    Used taking damage as an example

--- game/gamestates/animations/take_damage.lua
+++ game/gamestates/animations/take_damage.lua
@@ -1,10 +1,14 @@
 
 -- luacheck: globals love MAIN_TIMER
 
+local VIEWDEFS    = require 'view.definitions'
 local RisingText  = require 'view.sector.risingtext'
 local BodyView    = require 'view.sector.bodyview'
+local AnimationFX = require 'view.helpers.animationfx'
 local COLORS      = require 'domain.definitions.colors'
 local PLAYSFX     = require 'helpers.playsfx'
+
+local vec2        = require 'cpml' .vec2
 
 local ANIM = require 'common.activity' ()
 
@@ -15,6 +19,9 @@ function ANIM:script(_, view, report)
   if sectorview:isInsideFov(i, j) then
     local bodyview = sectorview:getBodyView(body)
     local damage_text = ("-%d"):format(report.amount)
+    local x, y = bodyview:getScreenPosition():unpack()
+    local w, h = VIEWDEFS.VIEWPORT_DIMENSIONS()
+    AnimationFX("fx-bonk", vec2(x + w/2, y + h/2 - VIEWDEFS.TILE_H/2))
     if report.sfx then PLAYSFX(report.sfx) end
     RisingText(bodyview, damage_text, COLORS.NOTIFICATION):play()
     local source_pos = BodyView.tileToScreen(report.source:getPos())
