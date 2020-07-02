c8d49896c7cfb8c4258c7a693e0635320df6dbc2
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-07-02 17:34:06 -0300

    Close #1425 and fix #1423

--- game/database/schema/drop.lua
+++ game/database/schema/drop.lua
@@ -3,6 +3,9 @@ return {
   { id = 'name', name = "Name", type = 'string' },
   { id = "sprite", name = "Sprite", type = 'enum',
     options = 'resources.sprite' },
-  { id = 'ability', name = "Triggered Ability", type = 'ability' }
+  { id = 'ability', name = "Triggered Ability", type = 'ability' },
+  { id = 'sfx', name = "SFX", type = 'enum',
+    options = 'resources.sfx',
+    optional = true },
 }
 
