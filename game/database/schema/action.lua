e24fb19264100691fb907c121d8f24733f633d65
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-07-01 22:24:42 -0300

    Remove signature abilities from the code

--- game/database/schema/action.lua
+++
@@ -1,9 +0,0 @@
-
-local DEFS = require 'domain.definitions'
-
-return {
-  { id = 'cost', name = "Cost", type = 'range', min = 0,
-    max = DEFS.ACTION.MAX_FOCUS },
-  { id = 'ability', name = "Activated Ability", type = 'ability' },
-}
-
