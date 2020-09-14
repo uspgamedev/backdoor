788f22eaff4db2773ffdd109939212dbfb8d1862
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-09-14 15:17:22 -0300

    Bump version

--- game/setup.lua
+++ game/setup.lua
@@ -11,7 +11,7 @@ local setup = {}
 --Set game's global variables, random seed, window configuration and anything else needed
 function setup.config()
 
-    VERSION = "0.7.0"
+    VERSION = "0.9.0"
     DEV = true
 
     --RANDOM SEED--
