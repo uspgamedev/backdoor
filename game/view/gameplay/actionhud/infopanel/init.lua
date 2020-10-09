f86861b8d65ef3271c34e3050c397a2c8876c026
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-10-09 19:01:07 -0300

    Experiment with header

--- game/view/gameplay/actionhud/infopanel/init.lua
+++ game/view/gameplay/actionhud/infopanel/init.lua
@@ -9,6 +9,7 @@ local InfoPanel = Class{
 }
 
 local _WIDTH = 200
+local _HEADER_HEIGHT = 40
 local _HEIGHT = 200
 
 function InfoPanel:init(position)
@@ -26,12 +27,15 @@ function InfoPanel:draw()
   local g = love.graphics -- luacheck: globals love
   g.push()
   g.translate(self.position:unpack())
+  g.setColor(VIEW_COLORS.BRIGHT)
+  g.rectangle('fill', -2, 0, _WIDTH+4, _HEADER_HEIGHT+4, 4, 4)
   g.setColor(VIEW_COLORS.DARK)
-  g.rectangle('fill', 0, 0, _WIDTH, _HEIGHT, 4, 4)
+  g.print("Information", 16, 8)
+  g.rectangle('fill', 0, _HEADER_HEIGHT, _WIDTH, _HEIGHT, 4, 4)
   g.setColor(VIEW_COLORS.BRIGHT)
   g.setLineWidth(4)
-  g.rectangle('line', 0, 0, _WIDTH, _HEIGHT, 4, 4)
-  g.print("This is a test", 16, 16)
+  g.rectangle('line', 0, _HEADER_HEIGHT, _WIDTH, _HEIGHT, 4, 4)
+  g.print("This is a test", 16, _HEADER_HEIGHT + 16)
   g.pop()
 end
 
