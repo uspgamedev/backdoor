28e7ca427fd7c47162e2f16d81cff85662886448
Rica <pudimdefrutas@gmail.com>
2020-10-30 15:19:47 -0300

    Using weighted distribution for pack generation

--- game/domain/pack.lua
+++ game/domain/pack.lua
@@ -12,19 +12,24 @@ function PACK.generatePackFrom(collection_name)
   if not collection_name then return _EMPTY end
   local collection = DB.loadSpec('collection', collection_name)
   local pack = {}
+  local total_weight = 0
+  for _,card_drop in pairs(collection.cards) do
+    total_weight = total_weight + card_drop.drop
+  end
   local n = 0
   while n < DEFS.PACK_SIZE do
+    local p = RANDOM.generate(0, total_weight)
+    local cur_weight = 0
     for _,card_drop in pairs(collection.cards) do
-      local p = RANDOM.generate(1, 100)
-      if p <= card_drop.drop then
+      cur_weight = cur_weight + card_drop.drop
+      if p <= cur_weight then
         n = n + 1
         pack[n] = CARDSET.getRandomCardFrom(card_drop.set)
+        break
       end
-      if n >= DEFS.PACK_SIZE then break end
     end
   end
   return pack
 end
 
 return PACK
-
